34837a3fe4b42267c4704eb787b6227fbbd9c949
carterm <carter.medlin@gmail.com>
2020-10-22 14:34:59 -0700

    slackbot works

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,7 +1,7 @@
 const { doDailyStatsPr } = require('./datasetUpdates');
+const { slackBotChatPost } = require('./slackBot');
 const { Values } = require('../local.settings.json');
 Object.keys(Values).forEach(x=>process.env[x]=Values[x]); //Load local settings file for testing
-
 
 const { queryDataset, getDatabaseConnection } = require('./snowflakeQuery');
 
@@ -11,8 +11,15 @@ const { queryDataset, getDatabaseConnection } = require('./snowflakeQuery');
 const masterbranch='master', stagingbranch='staging';
 const mergetargets = [masterbranch,stagingbranch];
 
-  await doDailyStatsPr(mergetargets);
+  //await doDailyStatsPr(mergetargets);
 
+
+  const targetChannel = 'C01DBP67MSQ'; // 'C01AA1ZB05B';
+  const response = await slackBotChatPost(targetChannel,'#TEST#\n##Test 2##\n###Test 3###');
+
+  const json = await response.json();
+
+  console.log(JSON.stringify(json,null,2));
 
 
   //const sqlText = `SELECT TOP 1 * from COVID.PRODUCTION.VW_TABLEAU_COVID_METRICS_STATEWIDE ORDER BY DATE DESC`;
---
+++ sandbox/slackBot/index.js
@@ -0,0 +1,38 @@
+const fetch = require('node-fetch');
+const slackApiChatPost = 'https://slack.com/api/chat.postMessage';
+
+const slackBotGetToken = () => {
+  const token = process.env["SLACKBOT_TOKEN"];
+
+  if (!token) {
+    //developers that don't set the creds can still use the rest of the code
+    console.error('You need local.settings.json to contain "SLACKBOT_TOKEN" to use slackbot features.');
+    return;
+  }
+
+  return token;
+}
+
+const slackApiPost = bodyJSON =>
+    ({
+        method: 'POST',
+        headers: {
+          'Authorization' : `Bearer ${slackBotGetToken()}`,
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(bodyJSON)
+    });
+
+const slackBotChatPost = async (channel,text,attachments) => {
+  const payload = {
+    channel,
+    text,
+    attachments
+  }
+
+  return await fetch(slackApiChatPost,slackApiPost(payload));
+}
+
+module.exports = {
+  slackBotChatPost
+}
