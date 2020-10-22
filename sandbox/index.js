ce5a9905318ebb2361471716d1f675dc0f62337d
carterm <carter.medlin@gmail.com>
2020-10-22 11:57:43 -0700

    connection management

--- sandbox/index.js
+++ sandbox/index.js
@@ -2,8 +2,21 @@ const { doDailyStatsPr } = require('./datasetUpdates');
 const { Values } = require('../local.settings.json');
 Object.keys(Values).forEach(x=>process.env[x]=Values[x]); //Load local settings file for testing
 
+
+const { queryDataset, getDatabaseConnection } = require('./snowflakeQuery');
+
+(async () => {
+
 //const masterbranch='synctest3', stagingbranch='synctest3_staging';
 const masterbranch='master', stagingbranch='staging';
 const mergetargets = [masterbranch,stagingbranch];
 
-doDailyStatsPr(mergetargets);
+  //await doDailyStatsPr(mergetargets);
+
+
+
+  const sqlText = `SELECT TOP 1 * from COVID.PRODUCTION.VW_TABLEAU_COVID_METRICS_STATEWIDE ORDER BY DATE DESC`;
+  console.log(JSON.stringify(await queryDataset(sqlText),null,2));
+
+  //console.log(JSON.stringify(await queryDataset(sqlText),null,2));
+})();
