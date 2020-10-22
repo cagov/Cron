c688c7e73cc389acf138dd56a42cc03c39cbee21
carterm <carter.medlin@gmail.com>
2020-10-22 11:23:32 -0700

    removing check flags

--- sandbox/index.js
+++ sandbox/index.js
@@ -2,11 +2,8 @@ const { doDailyStatsPr } = require('./datasetUpdates');
 const { Values } = require('../local.settings.json');
 Object.keys(Values).forEach(x=>process.env[x]=Values[x]); //Load local settings file for testing
 
-//const masterbranch='synctest3', stagingbranch='synctest3_staging', checkStatsPrs = false;
-const masterbranch='master', stagingbranch='staging', checkStatsPrs = true;
+//const masterbranch='synctest3', stagingbranch='synctest3_staging';
+const masterbranch='master', stagingbranch='staging';
 const mergetargets = [masterbranch,stagingbranch];
 
-//Check to see if we need stats update PRs
-if (checkStatsPrs) {
-  doDailyStatsPr(mergetargets);
-}
+doDailyStatsPr(mergetargets);
