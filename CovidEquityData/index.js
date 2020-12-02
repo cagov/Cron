180cd0a598673b62771438e803997886e7cf56ac
carterm <carter.medlin@gmail.com>
2020-12-01 14:09:09 -1000

    covid eq queries

--- CovidEquityData/index.js
+++ CovidEquityData/index.js
@@ -38,114 +38,60 @@ module.exports = async function (context, functionInput) {
             }
         });
         
-        // cumulative for R/E per 100K, R/E by % pop. there used to be a REPORT_DATE here and we used to have to do where REPORT_DATE = (select max(REPORT_DATE) from PRODUCTION.VW_CDPH_DEMOGRAPHIC_RATE_CUMULATIVE); but that has been removed and we expect a single cumulative value here now
-        const cumulativeData = new Promise((resolve, reject) => {
+        const DbSqlWork = {
+            // cumulative for R/E per 100K, R/E by % pop. there used to be a REPORT_DATE here and we used to have to do where REPORT_DATE = (select max(REPORT_DATE) from PRODUCTION.VW_CDPH_DEMOGRAPHIC_RATE_CUMULATIVE); but that has been removed and we expect a single cumulative value here now
+            cumulativeData : `select COUNTY, DEMOGRAPHIC_SET, DEMOGRAPHIC_SET_CATEGORY, METRIC, METRIC_VALUE, METRIC_VALUE_PER_100K, APPLIED_SUPPRESSION, POPULATION_PERCENTAGE, METRIC_TOTAL_PERCENTAGE, METRIC_VALUE_30_DAYS_AGO, METRIC_VALUE_PER_100K_30_DAYS_AGO, METRIC_VALUE_PER_100K_DELTA_FROM_30_DAYS_AGO, METRIC_TOTAL_PERCENTAGE_30_DAYS_AGO, METRIC_VALUE_PERCENTAGE_DELTA_FROM_30_DAYS_AGO from PRODUCTION.VW_CDPH_DEMOGRAPHIC_RATE_CUMULATIVE where DEMOGRAPHIC_SET = 'race_ethnicity'`,
+            cumulativeStatewideData : `select COUNTY, DEMOGRAPHIC_SET, DEMOGRAPHIC_SET_CATEGORY, METRIC, METRIC_VALUE, METRIC_VALUE_PER_100K, APPLIED_SUPPRESSION, POPULATION_PERCENTAGE, METRIC_TOTAL_PERCENTAGE, METRIC_VALUE_30_DAYS_AGO, METRIC_VALUE_PER_100K_30_DAYS_AGO, METRIC_VALUE_PER_100K_DELTA_FROM_30_DAYS_AGO, METRIC_TOTAL_PERCENTAGE_30_DAYS_AGO, METRIC_VALUE_PERCENTAGE_DELTA_FROM_30_DAYS_AGO from PRODUCTION.VW_CDPH_DEMOGRAPHIC_RATE_CUMULATIVE where DEMOGRAPHIC_SET = 'Combined'`,
+            // statewide stats for comparison
+            statewideData : `select AGE_GROUP, GENDER, RACE_ETHNICITY, POPULATION, SF_LOAD_TIMESTAMP from COVID.PRODUCTION.CDPH_STATIC_DEMOGRAPHICS where SF_LOAD_TIMESTAMP = (select max(SF_LOAD_TIMESTAMP) from PRODUCTION.CDPH_STATIC_DEMOGRAPHICS)`,
+            missingnessData : `select COUNTY, METRIC, MISSING, NOT_MISSING, TOTAL, PERCENT_COMPLETE, PERCENT_COMPLETE_30_DAYS_PRIOR, PERCENT_COMPLETE_30_DAYS_DIFF, REPORT_DATE from PRODUCTION.VW_CDPH_DEMOGRAPHIC_COMPLETENESS where REPORT_DATE = (select max(REPORT_DATE) from PRODUCTION.VW_CDPH_DEMOGRAPHIC_COMPLETENESS)`,
+            // missingness sexual orientation, gender identity
+            missingnessSOGIData : `select COUNTY, SOGI_CATEGORY, METRIC, MISSING, NOT_MISSING, TOTAL,PERCENT_COMPLETE, PERCENT_COMPLETE_30_DAYS_AGO, DIFF_30_DAY,REPORT_DATE from PRODUCTION.VW_CDPH_SOGI_COMPLETENESS where REPORT_DATE = (select max(REPORT_DATE) from PRODUCTION.VW_CDPH_DEMOGRAPHIC_COMPLETENESS)`,
+            socialData : `select DATE, SOCIAL_DET, SOCIAL_TIER, SORT, CASES_7DAYAVG_7DAYSAGO, POPULATION, CASE_RATE_PER_100K, STATE_CASE_RATE_PER_100K, CASE_RATE_PER_100K_30_DAYS_AGO, RATE_DIFF_30_DAYS from PRODUCTION.VW_CDPH_CASE_RATE_BY_SOCIAL_DET where DATE = (select max(DATE) from PRODUCTION.VW_CDPH_CASE_RATE_BY_SOCIAL_DET)`,
+            // equity metric line chart
+            healthEquityData : `select COUNTY, DATE, METRIC, METRIC_VALUE, METRIC_VALUE_30_DAYS_AGO, METRIC_VALUE_DIFF from COVID.PRODUCTION.VW_EQUITY_METRIC_POS_30_DAY_BY_CNT`,
+        };
+
+        const executeSql = async (sqlWork) => {
+            const promises = [];
+
+            for(name of Object.keys(sqlWork)) {
+                promises.push(getDbPromise(name,sqlWork[name]));
+            }
+
+            const resultDatasets = await Promise.all(promises);
+
+            const result = {};
+
+            //flatten results
+            for(promiseResult of resultDatasets) {
+                for(name of Object.keys(promiseResult)) {
+                    result[name] = promiseResult[name];
+                }
+            }
+
+            return result;
+        }
+
+
+        const getDbPromise = (name, sqlText) => new Promise((resolve, reject) => {
             connection.execute({
-                sqlText: `select COUNTY, DEMOGRAPHIC_SET, DEMOGRAPHIC_SET_CATEGORY, METRIC, METRIC_VALUE, METRIC_VALUE_PER_100K, APPLIED_SUPPRESSION, POPULATION_PERCENTAGE, METRIC_TOTAL_PERCENTAGE, METRIC_VALUE_30_DAYS_AGO, METRIC_VALUE_PER_100K_30_DAYS_AGO, METRIC_VALUE_PER_100K_DELTA_FROM_30_DAYS_AGO, METRIC_TOTAL_PERCENTAGE_30_DAYS_AGO, METRIC_VALUE_PERCENTAGE_DELTA_FROM_30_DAYS_AGO from PRODUCTION.VW_CDPH_DEMOGRAPHIC_RATE_CUMULATIVE where DEMOGRAPHIC_SET = 'race_ethnicity'`,
+                sqlText,
                 complete: function(err, stmt, rows) {
                     if (err) {
                         console.error('Failed to execute statement due to the following error: ' + err.message);
+                        reject('Failed to execute statement due to the following error: ' + err.message);
                     } else {
                         console.log('Successfully executed statement: ' + stmt.getSqlText());
+                        const result = {};
+                        result[name] = rows;
+                        resolve(result);
                     }
-                    resolve({"cumulative": rows})
                 }
             });
         });
 
-        const cumulativeStatewideData = new Promise((resolve, reject) => {
-            connection.execute({
-                sqlText: `select COUNTY, DEMOGRAPHIC_SET, DEMOGRAPHIC_SET_CATEGORY, METRIC, METRIC_VALUE, METRIC_VALUE_PER_100K, APPLIED_SUPPRESSION, POPULATION_PERCENTAGE, METRIC_TOTAL_PERCENTAGE, METRIC_VALUE_30_DAYS_AGO, METRIC_VALUE_PER_100K_30_DAYS_AGO, METRIC_VALUE_PER_100K_DELTA_FROM_30_DAYS_AGO, METRIC_TOTAL_PERCENTAGE_30_DAYS_AGO, METRIC_VALUE_PERCENTAGE_DELTA_FROM_30_DAYS_AGO from PRODUCTION.VW_CDPH_DEMOGRAPHIC_RATE_CUMULATIVE where DEMOGRAPHIC_SET = 'Combined'`,
-                complete: function(err, stmt, rows) {
-                    if (err) {
-                        console.error('Failed to execute statement due to the following error: ' + err.message);
-                    } else {
-                        console.log('Successfully executed statement: ' + stmt.getSqlText());
-                    }
-                    resolve({"cumulative": rows})
-                }
-            });
-        });
-
-        // statewide stats for comparison
-        const statewideData = new Promise((resolve, reject) => {
-            connection.execute({
-                sqlText: `select AGE_GROUP, GENDER, RACE_ETHNICITY, POPULATION, SF_LOAD_TIMESTAMP from COVID.PRODUCTION.CDPH_STATIC_DEMOGRAPHICS where SF_LOAD_TIMESTAMP = (select max(SF_LOAD_TIMESTAMP) from PRODUCTION.CDPH_STATIC_DEMOGRAPHICS)`,
-                complete: function(err, stmt, rows) {
-                    if (err) {
-                        console.error('Failed to execute statement due to the following error: ' + err.message);
-                    } else {
-                        console.log('Successfully executed statement: ' + stmt.getSqlText());
-                    }
-                    resolve({"statewide": rows})
-                }
-            });
-        });
-
-        const missingnessData = new Promise((resolve, reject) => {
-            let sqlStatement = `select COUNTY, METRIC, MISSING, NOT_MISSING, TOTAL, PERCENT_COMPLETE, PERCENT_COMPLETE_30_DAYS_PRIOR, PERCENT_COMPLETE_30_DAYS_DIFF, REPORT_DATE from PRODUCTION.VW_CDPH_DEMOGRAPHIC_COMPLETENESS where REPORT_DATE = (select max(REPORT_DATE) from PRODUCTION.VW_CDPH_DEMOGRAPHIC_COMPLETENESS)`;
-            connection.execute({
-                sqlText: sqlStatement,
-                complete: function(err, stmt, rows) {
-                    if (err) {
-                        console.error('Failed to execute statement due to the following error: ' + err.message);
-                    } else {
-                        console.log('Successfully executed statement: ' + stmt.getSqlText());
-                    }
-                    resolve({"missingness": rows})
-                }
-            });
-        });
-        // missingness sexual orientation, gender identity
-        const missingnessSOGIData = new Promise((resolve, reject) => {
-            let sqlStatement = `select COUNTY, SOGI_CATEGORY, METRIC, MISSING, NOT_MISSING, TOTAL,PERCENT_COMPLETE, PERCENT_COMPLETE_30_DAYS_AGO, DIFF_30_DAY,REPORT_DATE from PRODUCTION.VW_CDPH_SOGI_COMPLETENESS where REPORT_DATE = (select max(REPORT_DATE) from PRODUCTION.VW_CDPH_DEMOGRAPHIC_COMPLETENESS)`;
-            connection.execute({
-                sqlText: sqlStatement,
-                complete: function(err, stmt, rows) {
-                    if (err) {
-                        console.error('Failed to execute statement due to the following error: ' + err.message);
-                    } else {
-                        console.log('Successfully executed statement: ' + stmt.getSqlText());
-                    }
-                    resolve({"missingnessSOGI": rows})
-                }
-            });
-        });
-
-        const socialData = new Promise((resolve, reject) => {
-            connection.execute({
-                sqlText: `select DATE, SOCIAL_DET, SOCIAL_TIER, SORT, CASES_7DAYAVG_7DAYSAGO, POPULATION, CASE_RATE_PER_100K, STATE_CASE_RATE_PER_100K, CASE_RATE_PER_100K_30_DAYS_AGO, RATE_DIFF_30_DAYS from PRODUCTION.VW_CDPH_CASE_RATE_BY_SOCIAL_DET where DATE = (select max(DATE) from PRODUCTION.VW_CDPH_CASE_RATE_BY_SOCIAL_DET)`,
-                complete: function(err, stmt, rows) {
-                    if (err) {
-                        console.error('Failed to execute statement due to the following error: ' + err.message);
-                    } else {
-                        console.log('Successfully executed statement: ' + stmt.getSqlText());
-                    }
-                    resolve({"social": rows})
-                }
-            });
-        });	
-        
-        // equity metric line chart
-        const healthEquityData = new Promise((resolve, reject) => {
-            connection.execute({
-                sqlText: `select COUNTY, DATE, METRIC, METRIC_VALUE, METRIC_VALUE_30_DAYS_AGO, METRIC_VALUE_DIFF from COVID.PRODUCTION.VW_EQUITY_METRIC_POS_30_DAY_BY_CNT`,
-                complete: function(err, stmt, rows) {
-                    if (err) {
-                        console.error('Failed to execute statement due to the following error: ' + err.message);
-                    } else {
-                        console.log('Successfully executed statement: ' + stmt.getSqlText());
-                    }
-                    resolve({"healthequity": rows})
-                }
-            });
-        });	
-
-        let allData = {};
-
-        await Promise.all([missingnessData, cumulativeData, socialData, statewideData, missingnessSOGIData, healthEquityData, cumulativeStatewideData]).then((values) => {
-            allData = values;
-        });
+        const allData = await executeSql(DbSqlWork);
 
         let reviewBranchName = `data-${new Date().toISOString().split('T')[0]}-equitydash-2-review`;
         let reviewCompletedBranchName = `data-${new Date().toISOString().split('T')[0]}-equitydash-review-complete`;
@@ -174,7 +120,7 @@ module.exports = async function (context, functionInput) {
 
         let allFilesMap = new Map();
         // this is combining cases, testing and deaths metrics
-        allData[0].missingness.forEach(item => {
+        allData.missingnessData.forEach(item => {
             let mapKey = `missingness-${item.COUNTY}`;
             let countyInfo = allFilesMap.get(mapKey);
             if(!countyInfo) {
@@ -185,7 +131,7 @@ module.exports = async function (context, functionInput) {
             allFilesMap.set(mapKey,countyInfo)
         })
         // combining sogi missingness with regular missingness so I can write less files
-        allData[4].missingnessSOGI.forEach(item => {
+        allData.missingnessSOGIData.forEach(item => {
             let mapKey = `missingness-${item.COUNTY}`;
             let countyInfo = allFilesMap.get(mapKey);
             if(!countyInfo) {
@@ -207,7 +153,7 @@ module.exports = async function (context, functionInput) {
 
 
         // for cumulative go through all, add each county to map with cumulative key, all records for that county should be in that one file
-        allData[1].cumulative.forEach(item => {
+        allData.cumulativeData.forEach(item => {
             let mapKey = `cumulative-${item.COUNTY}`;
             let countyInfo = allFilesMap.get(mapKey);
             if(!countyInfo) {
@@ -216,7 +162,7 @@ module.exports = async function (context, functionInput) {
             item.SORT_METRIC = item.METRIC_TOTAL_PERCENTAGE / item.POPULATION_PERCENTAGE;
             item.METRIC_TOTAL_DELTA = 100 - item.METRIC_TOTAL_PERCENTAGE;
             item.POPULATION_PERCENTAGE_DELTA = 100 - item.POPULATION_PERCENTAGE;
-            let allMetricItemsInCounty = [...allData[1].cumulative].filter(f => f.COUNTY === item.COUNTY && f.METRIC === item.METRIC);
+            let allMetricItemsInCounty = [...allData.cumulativeData].filter(f => f.COUNTY === item.COUNTY && f.METRIC === item.METRIC);
             item.WORST_VALUE = allMetricItemsInCounty.reduce((a, e ) => e["METRIC_VALUE_PER_100K"] > a["METRIC_VALUE_PER_100K"] ? e : a).METRIC_VALUE_PER_100K;
             item.WORST_VALUE_DELTA = item.WORST_VALUE - item.METRIC_VALUE_PER_100K;
             let nonNulls = allMetricItemsInCounty.filter(f => f["METRIC_VALUE_PER_100K"] != null);
@@ -232,7 +178,7 @@ module.exports = async function (context, functionInput) {
         })
 
         // social data should all go in one file
-        allData[2].social.forEach(item => {
+        allData.socialData.forEach(item => {
             let mapKey = `social-data-${item.SOCIAL_DET}`;
             let countyInfo = allFilesMap.get(mapKey);
             if(!countyInfo) {
@@ -244,7 +190,7 @@ module.exports = async function (context, functionInput) {
         allFilesMap.set('temp-test-file','file contents '+new Date().getTime())
 
         // healthequity data
-        allData[5].healthequity.forEach(item => {
+        allData.healthEquityData.forEach(item => {
             let mapKey = `healthequity-${item.COUNTY}`;
             let countyInfo = allFilesMap.get(mapKey);
             if(!countyInfo) {
@@ -257,7 +203,7 @@ module.exports = async function (context, functionInput) {
             allFilesMap.set(mapKey,countyInfo)
         })
 
-        allData[6].cumulative.forEach(item => {
+        allData.cumulativeStatewideData.forEach(item => {
             let info = allFilesMap.get('cumulative-combined');
             if(!info) {
                 info = {};
@@ -271,7 +217,7 @@ module.exports = async function (context, functionInput) {
         // write one file for statewide data
         let statewideMapKey = `statewide-data`;
         let statewidePopData = [];
-        allData[3].statewide.forEach(item => {
+        allData.statewideData.forEach(item => {
             statewidePopData.push(item)
         })
         allFilesMap.set(statewideMapKey,statewidePopData)
