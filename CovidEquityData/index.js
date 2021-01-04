a2efb8752c0032d28285b94e9dc45fe624c59b15
carterm <carter.medlin@gmail.com>
2021-01-04 13:15:54 -0800

    setting up debug

--- CovidEquityData/index.js
+++ CovidEquityData/index.js
@@ -3,11 +3,12 @@ const { slackBotChatPost, slackBotDelayedChatPost, slackBotReportError } = requi
 const githubBranch = "master";
 const stagingFileLoc = 'data/to-review/equitydash/';
 const productionFileLoc = 'data/reviewed/equitydash/';
-const branchPrefix = 'data-';
+const branchPrefix = 'carter-data-';
 const slackBotCompletedWorkChannel = 'C01BMCQK0F6'; //main channel
 //const slackBotDebugChannel = 'C0112NK978D'; //Aaron debug?
 //const slackBotCompletedWorkChannel = 'C01DBP67MSQ'; //Carter debug
-const slackBotDebugChannel = 'C01DBP67MSQ'; //Carter debug
+//const slackBotDebugChannel = 'C01DBP67MSQ'; //#testingbot
+const slackBotDebugChannel = 'C01H6RB99E2'; //#Carter debug
 const appName = 'CovidEquityData';
 
 const {
