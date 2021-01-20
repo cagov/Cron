318ad5330ec7d29a2c68f7296c173c66d32736bc
carterm <carter.medlin@gmail.com>
2021-01-19 16:59:18 -0800

    simplify news feed

--- CovidNewsFeed/index.js
+++ CovidNewsFeed/index.js
@@ -1,17 +1,22 @@
 const md5 = require('md5');
-const githubApiUrl = "https://api.github.com/repos/cagov/covid19/";
 const githubBranch = "master";
 const govArticleLoc = 'pages/guidancefeed/gov';
 const cdphArticleLoc = 'pages/guidancefeed/cdph';
-const addToGithub = require('./write/git.js');
 const createGovItem = require('./templates/gov.js');
 const createCDPHItem = require('./templates/cdph.js');
 const govNews = require("./sources/gov.js");
 const cdphNews = require("./sources/cdph.js");
-const gitNews = require("./sources/git.js");
-const debugChannel = 'C01DBP67MSQ'; // 'C01AA1ZB05B';
+const debugChannel = 'C01DBP67MSQ'; // #testingbot;
 const appName = 'CovidNewsFeed';
 const { slackBotReportError } = require('../common/slackBot');
+// https://github-tools.github.io/github/docs/3.2.3/Repository.html
+const GitHub = require('github-api'); 
+const githubUser = 'cagov';
+const githubRepo = 'covid19';
+const committer = {
+  name: process.env["GITHUB_NAME"],
+  email: process.env["GITHUB_EMAIL"]
+};
 
 const getGovNews = new Promise((resolve, reject) => {
   govNews(resolve,reject);
@@ -19,58 +24,50 @@ const getGovNews = new Promise((resolve, reject) => {
 const getCDPHNews = new Promise((resolve, reject) => {
   cdphNews(resolve,reject);
 });
-const getExistingGovNews = new Promise((resolve, reject) => {
-  gitNews(resolve,reject, githubBranch, githubApiUrl, govArticleLoc);
-});
-const getExistingCDPHNews = new Promise((resolve, reject) => {
-  gitNews(resolve,reject, githubBranch, githubApiUrl, cdphArticleLoc);
-});
+
 
 module.exports = async function (context, myTimer) {
   //await slackBotChatPost(debugChannel,`${appName} started`);
 
   try {
+    const gitModule = new GitHub({ token: process.env["GITHUB_TOKEN"] });
+    const gitRepo = await gitModule.getRepo(githubUser,githubRepo);
+    const getExistingGovNews = gitRepo.getContents(githubBranch,govArticleLoc,true);
+    const getExistingCDPHNews = gitRepo.getContents(githubBranch,cdphArticleLoc,true);
+
   await Promise.all([getGovNews, getExistingGovNews, getCDPHNews, getExistingCDPHNews]).then( 
     async values => {
       let writtenFileCount = 0;
-      let newGovItems = values[0].filter(newItem => {
-        return values[1].filter(old => {
-          return old.name === `${newItem.id}.html`;
-        }).length < 1;
-      });
+      let newGovItems = values[0].filter(newItem => 
+        !values[1].data.some(old => 
+          old.name === `${newItem.id}.html`
+        )
+      );
 
-      for(let i = 0;i<newGovItems.length;i++) {
-        let item = newGovItems[i];
-        await new Promise((resolve, reject) => {
-          addToGithub(createGovItem(item), githubBranch, githubApiUrl, govArticleLoc, result => {
-            if(result) {
-              writtenFileCount++;
-              resolve(result);
-            } else {
-              reject(result);
-            }
-          });
-        });
+      for (let item of newGovItems) {
+        const newFile = createGovItem(item);
+        const newFilePath = `${govArticleLoc}/${newFile.filename}.html`;
+        const commitMessage = `ADD ${newFilePath}`;
+        console.log(commitMessage);
+
+        await gitRepo.writeFile(githubBranch, newFilePath, newFile.html, commitMessage, {committer,encode:true});
+        writtenFileCount++;
       }
 
-      let newCDPHItems = values[2].filter(newItem => {
-        return values[3].filter(old => {
-          return old.name === `${md5(newItem.url)}.html`;
-        }).length < 1;
-      });
+      let newCDPHItems = values[2].filter(newItem => 
+        !values[3].data.some(old => 
+          old.name === `${md5(newItem.url)}.html`
+        )
+      );
 
-      for(let i = 0;i<newCDPHItems.length;i++) {
-        let item = newCDPHItems[i];
-        await new Promise((resolve, reject) => {
-          addToGithub(createCDPHItem(item), githubBranch, githubApiUrl, cdphArticleLoc, result => {
-            if(result) {
-              writtenFileCount++;
-              resolve(result);
-            } else {
-              reject(result);
-            }
-          });
-        });
+      for (let item of newCDPHItems) {
+        const newFile = createCDPHItem(item);
+        const newFilePath = `${cdphArticleLoc}/${newFile.filename}.html`;
+        const commitMessage = `ADD ${newFilePath}`;
+        console.log(commitMessage);
+
+        await gitRepo.writeFile(githubBranch, newFilePath, newFile.html, commitMessage, {committer,encode:true});
+        writtenFileCount++;
       }
       // respond with success here
       console.log(`Done, wrote - ${writtenFileCount}`);
@@ -78,7 +75,6 @@ module.exports = async function (context, myTimer) {
     }
   );
 } catch (e) {
-  //supressing errors until cdph site is fixed
   await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
 }
 };
