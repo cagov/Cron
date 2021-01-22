e1227e10e004fc4721bcd693635c46c5e590a994
carterm <carter.medlin@gmail.com>
2021-01-22 10:50:18 -0800

    configuring env variables

--- TestConn/index.js
+++ TestConn/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context) {
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: JSON.stringify(process)
+        body: JSON.stringify(process.env,null,2)
     };
 };
