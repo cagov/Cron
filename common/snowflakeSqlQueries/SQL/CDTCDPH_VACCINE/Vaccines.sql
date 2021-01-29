840e19cb1a761e920396762107b113361f45a204
carterm <carter.medlin@gmail.com>
2021-01-29 12:32:27 -0800

    Working with existing sql

---
+++ common/snowflakeSqlQueries/SQL/CDTCDPH_VACCINE/Vaccines.sql
@@ -0,0 +1,4 @@
+  select
+    VACCINE_KPI_JSON
+  from
+    CA_VACCINE.CA_VACCINE.Vw_Website_kpi_Vaccines
--- common/snowflakeSqlQueries/index.js
+++ common/snowflakeSqlQueries/index.js
@@ -1,18 +1,21 @@
+const fs = require('fs');
+const path = `${__dirname}/SQL/`;
+const folders = fs.readdirSync(path);
 
+const result = {};
 
+folders.forEach(subpath=>{
+  const files = fs.readdirSync(path+subpath);
+  const container = {};
 
+  files.forEach(f => {
+    container[f.split('.')[0]]=fs.readFileSync(`${path}${subpath}/${f}`).toString();
+  });
 
+  result[subpath] = container;
+});
 
-
-
-
-
-
-
-
-
-module.exports = {
-  getDatabaseConnection,
-  queryDataset,
-  getDbPromise
-};
+/**
+ * Read all the SQL files and return them in a JSON structure by folder
+ */
+module.exports = result;
