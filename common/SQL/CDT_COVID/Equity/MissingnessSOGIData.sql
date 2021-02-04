cf20e5105a5ca6340136d89bd3a2b4fad3830931
carterm <carter.medlin@gmail.com>
2021-02-04 11:30:38 -0800

    MissingnesssSOGIData

--- common/SQL/CDT_COVID/Equity/MissingnessSOGIData.sql
+++ common/SQL/CDT_COVID/Equity/MissingnessSOGIData.sql
@@ -1,3 +1,9 @@
+-- Unknown data for Cases and Deaths for SO and GI
+-- 236 Rows
+-- SOGI_CATEGORY (gender_identity,sexual_orientation)  by
+--   by METRIC (cases,deaths)
+--   by COUNTY
+
 select
   COUNTY,
   SOGI_CATEGORY,
