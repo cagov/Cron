3473813df0b2690df688bfce6ed6d202393c842b
carterm <carter.medlin@gmail.com>
2021-03-08 13:07:50 -0800

    combined logic into existing fields

--- common/SQL/CDTCDPH_VACCINE/vaccine_hpi/vaccine_hpi.sql
+++ common/SQL/CDTCDPH_VACCINE/vaccine_hpi/vaccine_hpi.sql
@@ -3,20 +3,19 @@ select
     HPIQUARTILE,
     AGE16_POPULATION,
     FIRST_DOSE,
-    FIRST_DOSE_ONLY,
     COMPLETED_DOSE,
-    FIRST_DOSE_ONLY+COMPLETED_DOSE "COMBINED_DOSES",
-    FIRST_DOSE_ONLY/AGE16_POPULATION "FIRST_DOSE_RATIO",
+    COMBINED_DOSES,
+    FIRST_DOSE/AGE16_POPULATION "FIRST_DOSE_RATIO",
     COMPLETED_DOSE/AGE16_POPULATION "COMPLETED_DOSE_RATIO",
-    (FIRST_DOSE_ONLY+COMPLETED_DOSE)/AGE16_POPULATION "COMBINED_DOSES_RATIO"
+    COMBINED_DOSES/AGE16_POPULATION "COMBINED_DOSES_RATIO"
 from 
     (
         select
             MAX(case when DATE(ADMIN_DATE)>CURRENT_DATE() then NULL else DATE(ADMIN_DATE) end) "LATEST_ADMIN_DATE",
             HPIQUARTILE,
-            count(distinct case when dose_num='2' or VAX_LABEL='J&J' or recip_id in(select recip_id from CA_VACCINE.VW_TAB_INT_ALL where dose_num = '2' or VAX_LABEL='J&J') then null else recip_id end) "FIRST_DOSE_ONLY",
-            count(distinct case when dose_num='2' or VAX_LABEL='J&J' then null else recip_id end) "FIRST_DOSE",
+            count(distinct case when dose_num='2' or VAX_LABEL='J&J' or recip_id in(select recip_id from CA_VACCINE.VW_TAB_INT_ALL where dose_num = '2' or VAX_LABEL='J&J') then null else recip_id end) "FIRST_DOSE",
             count(distinct case when dose_num='2' or VAX_LABEL='J&J' then recip_id else null end) "COMPLETED_DOSE",
+            count(distinct vax_event_id) "COMBINED_DOSES",
             SUM(AGE16_POPULATION) "AGE16_POPULATION"
         from
             CA_VACCINE.VW_TAB_INT_ALL
