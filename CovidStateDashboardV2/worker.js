76225855b03489b7c653de8a38083defa8b3044e
carterm <carter.medlin@gmail.com>
2021-03-09 13:05:47 -0800

    broken in progress

--- CovidStateDashboardV2/worker.js
+++ CovidStateDashboardV2/worker.js
@@ -13,46 +13,68 @@ const masterBranch = 'master';
 const commitMessage = 'update Stats';
 const branchPrefix = 'auto-stats-update';
 
+const nowPacTime = options => new Date().toLocaleString("en-CA", {timeZone: "America/Los_Angeles", ...options});
+const todayDateString = () => nowPacTime({year: 'numeric',month: '2-digit',day: '2-digit'});
+const todayTimeString = () => nowPacTime({hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit'}).replace(/:/g,'-');
+
 //Check to see if we need stats update PRs, make them if we do.
 const doCovidStateDashboarV2 = async () => {
-    //uncomment this if you want to test the schema before running the query (schema dev mode)
-    //validateJSON('daily-stats-v2.json failed validation',null,schemaFileName,schemaTestGoodFilePath,schemaTestBadFilePath);    throw new Error('validation pass');
-    //validateJSON('CDTCDPH_VACCINE/Vaccines.sql failed validation', null,'../SQL/CDTCDPH_VACCINE/Vaccines.sql.Schema.json','../SQL/CDTCDPH_VACCINE/Vaccines.sql.Sample.json'); throw new Error('validation pass');
-
     const gitModule = new GitHub({ token: process.env["GITHUB_TOKEN"] });
     const gitRepo = await gitModule.getRepo(githubUser,githubRepo);
 
-    const todayDateString = new Date().toLocaleString("en-US", {year: 'numeric', month: 'numeric', day: 'numeric', timeZone: "America/Los_Angeles"}).replace(/\//g,'-');
-    const todayTimeString = new Date().toLocaleString("en-US", {hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: "America/Los_Angeles"}).replace(/:/g,'-');
-
-    const title = `${todayDateString} Stats Update`;
-    let branch = masterBranch;
-
-    const prs = await gitRepo.listPullRequests({
-        base:masterBranch
-    });
-    let Pr = prs.data.filter(x=>x.title===title)[0];
-
-    if(Pr) { //reuse the PR if it is still open
-        branch = Pr.head.ref;    
-    }
+    const title = `${todayDateString()} Stats Update`;
 
     const dataOutput = await getData_daily_stats_v2();
-    const targetcontent = (await gitRepo.getContents(branch,targetFileNameStats,true)).data;
+    const targetcontent = (await gitRepo.getContents(masterBranch,targetFileNameStats,true)).data;
     if(JSON.stringify(dataOutput.json.data)===JSON.stringify(targetcontent.data)) {
         console.log('data matched - no need to update');
     } else {
         console.log('data changed - updating');
-        if(!Pr) {
-            //new branch
-            branch = `${branchPrefix}-${todayDateString}-${todayTimeString}`;
-            await gitRepo.createBranch(masterBranch,branch);
-        }
 
+        const branch = `${branchPrefix}-${todayDateString()}-${todayTimeString()}`;
+        await gitRepo.createBranch(masterBranch,branch);
+        
+        await gitRepo.writeFile(branch, targetFileNameStats, JSON.stringify(dataOutput,null,2), commitMessage, {committer,encode:true});
+
+        //Create PR
+        const Pr = (await gitRepo.createPullRequest({
+            title,
+            head: branch,
+            base: masterBranch
+        }))
+        .data;
+        
+        //Approve the PR
+        await gitRepo.mergePullRequest(Pr.number,{
+            merge_method: 'squash'
+        });
+        //Delete PR branch
+        await gitRepo.deleteRef(`heads/${Pr.head.ref}`);
+        
+        return Pr;
+    }
+};
+
+const createPrForChange = async (gitRepo, Pr, contentArray) => {
+    for (const dataOutput of contentArray) {
+
+    }
+
+
+
+    const targetcontent = (await gitRepo.getContents(masterBranch,targetFileNameStats,true)).data;
+    if(JSON.stringify(dataOutput.json.data)===JSON.stringify(targetcontent.data)) {
+        console.log('data matched - no need to update');
+    } else {
+        console.log('data changed - updating');
+
+        const branch = `${branchPrefix}-${todayDateString()}-${todayTimeString()}`;
+        await gitRepo.createBranch(masterBranch,branch);
+        
         await gitRepo.writeFile(branch, targetFileNameStats, JSON.stringify(dataOutput,null,2), commitMessage, {committer,encode:true});
 
         if(!Pr) {
-            //new Pr
+            //Create PR
             Pr = (await gitRepo.createPullRequest({
                 title,
                 head: branch,
@@ -62,15 +84,16 @@ const doCovidStateDashboarV2 = async () => {
         }
     }
 
-    //Approve the PR
-    if(Pr) {
-        await gitRepo.mergePullRequest(Pr.number,{
-            merge_method: 'squash'
-        });
-
-        await gitRepo.deleteRef(`heads/${Pr.head.ref}`);
-    }
     return Pr;
+};
+
+const PrApprove = async (gitRepo, Pr) => {
+    //Approve the PR
+    await gitRepo.mergePullRequest(Pr.number,{
+        merge_method: 'squash'
+    });
+    //Delete PR branch
+    await gitRepo.deleteRef(`heads/${Pr.head.ref}`);
 };
 
 module.exports = {
