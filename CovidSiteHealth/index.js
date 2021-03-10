f8b95d10f214c1f75c7c28a6c7108d96aa9ba78d
carterm <carter.medlin@gmail.com>
2021-03-10 11:52:02 -0800

    temp message redirect for site health

--- CovidSiteHealth/index.js
+++ CovidSiteHealth/index.js
@@ -2,6 +2,7 @@ const { doHealthCheck } = require('./worker');
 const { slackBotChatPost, slackBotReportError } = require('../common/slackBot');
 const notifyChannel = 'C01DBP67MSQ'; //testingbot
 const debugChannel = 'C01DBP67MSQ'; //testingbot
+const tempChannel = 'C01H6RB99E2'; //carterDev
 const appName = 'CovidSiteHealth';
 
 module.exports = async function (context, myTimer) {
@@ -12,6 +13,10 @@ try {
     await slackBotChatPost(notifyChannel,`*${appName} Report*\n\`\`\`${JSON.stringify(report,null,2)}\`\`\``);
   }
 } catch (e) {
-  await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
+  if(e && e.stack && e.stack.includes('ECONNRESET')) {
+    await slackBotReportError(tempChannel,`Error running ${appName}`,e,context,myTimer);
+  } else {
+    await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
+  }
 }
 };
