ec34cafedca150aedb1b581636e159edd147355a
aaronhans <aaronhans@gmail.com>
2021-03-18 10:32:35 -0700

    uncomment the slackbot messages

--- CovidVaccineHPIV2/index.js
+++ CovidVaccineHPIV2/index.js
@@ -6,18 +6,18 @@ const debugChannel = 'C01DBP67MSQ'; // testingbot
 module.exports = async function (context, myTimer) {
   const appName = context.executionContext.functionName;
   try {
-    // await slackBotChatPost(debugChannel,`${appName} (Every 30 mins from 10:15am to 1:45pm)`);
+    await slackBotChatPost(debugChannel,`${appName} (Every 30 mins from 10:15am to 1:45pm)`);
 
     const PrResult = await doCovidVaccineHPIV2();
 
     if(PrResult) {
-      const prMessage = `Vaccine HPI data deployed\n${PrResult.html_url}`;
-    //   await slackBotChatPost(debugChannel,prMessage);
-    //   await slackBotChatPost(notifyChannel,prMessage);
+      const prMessage = `Vaccine HPI V2 data deployed\n${PrResult.html_url}`;
+      await slackBotChatPost(debugChannel,prMessage);
+      await slackBotChatPost(notifyChannel,prMessage);
     }
 
-    // await slackBotChatPost(debugChannel,`${appName} finished`);
+    await slackBotChatPost(debugChannel,`${appName} finished`);
   } catch (e) {
-    // await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
+    await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
   }
 };
