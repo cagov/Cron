024c9730f8fa7cf74c05e33ed32e356389038573
aaronhans <aaronhans@gmail.com>
2021-03-18 08:29:36 -0700

    initial new service

---
+++ CovidVaccineHPIV2/index.js
@@ -0,0 +1,23 @@
+const { doCovidVaccineHPIV2 } = require('./worker');
+const { slackBotChatPost, slackBotReportError } = require('../common/slackBot');
+const notifyChannel = 'C01HTTNKHBM'; //covid19-vaccines
+const debugChannel = 'C01DBP67MSQ'; // testingbot
+
+module.exports = async function (context, myTimer) {
+  const appName = context.executionContext.functionName;
+  try {
+    // await slackBotChatPost(debugChannel,`${appName} (Every 30 mins from 10:15am to 1:45pm)`);
+
+    const PrResult = await doCovidVaccineHPIV2();
+
+    if(PrResult) {
+      const prMessage = `Vaccine HPI data deployed\n${PrResult.html_url}`;
+    //   await slackBotChatPost(debugChannel,prMessage);
+    //   await slackBotChatPost(notifyChannel,prMessage);
+    }
+
+    // await slackBotChatPost(debugChannel,`${appName} finished`);
+  } catch (e) {
+    // await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
+  }
+};
---
+++ CovidVaccineHPIV2/worker.js
@@ -0,0 +1,116 @@
+const { queryDataset, getSQL } = require('../common/snowflakeQuery');
+const { validateJSON } = require('../common/schemaTester');
+const GitHub = require('github-api');
+const githubUser = 'cagov';
+const githubRepo = 'covid-static';
+const committer = {
+  name: process.env["GITHUB_NAME"],
+  email: process.env["GITHUB_EMAIL"]
+};
+const targetBranch = 'master';
+const SnowFlakeSqlPath = 'CDTCDPH_VACCINE/vaccine_hpi_v2/';
+const targetPath = 'data/vaccine-hpi/v2/';
+const targetFileName = 'vaccine-hpi.json';
+const schemaPath = `../SQL/${SnowFlakeSqlPath}schema/`;
+
+const nowPacTime = options => new Date().toLocaleString("en-CA", { timeZone: "America/Los_Angeles", ...options });
+const todayDateString = () => nowPacTime({ year: 'numeric', month: '2-digit', day: '2-digit' });
+const todayTimeString = () => nowPacTime({ hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' }).replace(/:/g, '-');
+
+const doCovidVaccineHPIV2 = async () => {
+  const gitModule = new GitHub({ token: process.env["GITHUB_TOKEN"] });
+  const gitRepo = await gitModule.getRepo(githubUser, githubRepo);
+
+  const branchPrefix = 'data-vaccine-hpi-v2-';
+  const commitMessage = 'Update Vaccine HPI Data';
+  const PrTitle = `${todayDateString()} Vaccine HPI`;
+  let branch = targetBranch;
+
+  const prs = await gitRepo.listPullRequests({
+    base: targetBranch
+  });
+  let Pr = prs.data.filter(x => x.title === PrTitle)[0];
+
+  if (Pr) { //reuse the PR if it is still open
+    branch = Pr.head.ref;
+  }
+
+  const dataOutput = await getData();
+  const targetcontent = (await gitRepo.getContents(branch, `${targetPath}${targetFileName}`, true)).data;
+  if (JSON.stringify(dataOutput.data) === JSON.stringify(targetcontent.data)) {
+    console.log('data matched - no need to update');
+  } else {
+    console.log('data changed - updating');
+
+    if (!Pr) {
+      //new branch
+      branch = `${branchPrefix}-${todayDateString()}-${todayTimeString()}`;
+      await gitRepo.createBranch(targetBranch, branch);
+    }
+
+    await gitRepo.writeFile(branch, `${targetPath}${targetFileName}`, JSON.stringify(dataOutput, null, 2), commitMessage, { committer, encode: true });
+
+    if (!Pr) {
+      //new Pr
+      Pr = (await gitRepo.createPullRequest({
+        title: PrTitle,
+        head: branch,
+        base: targetBranch
+      }))
+        .data;
+    }
+  }
+
+  Approve the PR
+  if(Pr) {
+      await gitRepo.mergePullRequest(Pr.number,{
+          merge_method: 'squash'
+      });
+
+      await gitRepo.deleteRef(`heads/${Pr.head.ref}`);
+  }
+  return Pr;
+};
+
+
+const getData = async () => {
+  const sqlResults = await queryDataset(
+    {
+      data: getSQL(`${SnowFlakeSqlPath}vaccine_hpi`),
+      doses: getSQL(`${SnowFlakeSqlPath}vaccine_doses`)
+    }
+    , process.env["SNOWFLAKE_CDTCDPH_VACCINE"]
+  );
+
+  // validateJSON('vaccine-hpi.json failed input validation', sqlResults.data,`${schemaPath}input/schema.json`,`${schemaPath}input/pass/`);
+
+  let LATEST_ADMINISTERED_DATE = new Date("1900-01-01");
+  sqlResults.data.forEach(r => {
+    if (LATEST_ADMINISTERED_DATE < r.LATEST_ADMIN_DATE) {
+      LATEST_ADMINISTERED_DATE = r.LATEST_ADMIN_DATE;
+    }
+    sqlResults.doses.forEach(doses => {
+      if (doses.HPIQUARTILE === r.HPIQUARTILE) {
+        r.COMBINED_DOSES = doses["COUNT(VAX_EVENT_ID)"]
+      }
+    })
+
+    delete r.LATEST_ADMIN_DATE;
+  });
+
+  const mappedResults = {
+    meta: {
+      LATEST_ADMINISTERED_DATE,
+      PUBLISHED_DATE: todayDateString()
+    },
+    data: sqlResults.data
+  };
+
+  validateJSON('vaccine-hpi.json failed output validation', mappedResults, `${schemaPath}output/schema.json`, `${schemaPath}output/pass/`);
+
+  return mappedResults;
+};
+
+module.exports = {
+  doCovidVaccineHPIV2
+};
