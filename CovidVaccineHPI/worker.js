541ecb0f90385e652a057ed13943e6b8bebf07a9
chachasikes <chachasikes@gmail.com>
2021-03-18 10:26:49 -0700

    probably broken refactor of a worker script showing suggestions on using config settings & making the code easier to read/fix/debug

--- CovidVaccineHPI/worker.js
+++ CovidVaccineHPI/worker.js
@@ -1,110 +1,211 @@
-const { queryDataset,getSQL } = require('../common/snowflakeQuery');
-const { validateJSON } = require('../common/schemaTester');
-const GitHub = require('github-api');
-const githubUser = 'cagov';
-const githubRepo = 'covid-static';
-const committer = {
-  name: process.env["GITHUB_NAME"],
-  email: process.env["GITHUB_EMAIL"]
+const { queryDataset, getSQL } = require("../common/snowflakeQuery");
+const { validateJSON } = require("../common/schemaTester");
+const GitHub = require("github-api");
+
+const getConfigGitHub = () => {
+  return {
+    githubUser: "cagov",
+    githubRepo: "covid-static",
+    masterBranch: "master",
+    targetPath: "data/vaccine-hpi/",
+    targetFileName: "vaccine-hpi.json",
+    outputFileName: "vaccine-hpi.json",
+    branchPrefix: "data-vaccine-hpi-",
+    branchCommitMessage: "Update Vaccine HPI Data",
+    committer: {
+      name: process.env["GITHUB_NAME"],
+      email: process.env["GITHUB_EMAIL"],
+    },
+  };
 };
-const masterBranch = 'master';
-const SnowFlakeSqlPath = 'CDTCDPH_VACCINE/vaccine_hpi/';
-const targetPath = 'data/vaccine-hpi/';
-const targetFileName = 'vaccine-hpi.json';
-const schemaPath = `../SQL/${SnowFlakeSqlPath}schema/`;
 
-const nowPacTime = options => new Date().toLocaleString("en-CA", {timeZone: "America/Los_Angeles", ...options});
-const todayDateString = () => nowPacTime({year: 'numeric',month: '2-digit',day: '2-digit'});
-const todayTimeString = () => nowPacTime({hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit'}).replace(/:/g,'-');
+const getConfigDataset = () => {
+  return {
+    SNOWFLAKE_TOKEN: process.env["SNOWFLAKE_CDTCDPH_VACCINE"],
+    SnowFlakeSqlPath: "CDTCDPH_VACCINE/vaccine_hpi/",
+    datasetName: "Vaccine HPI",
+    SnowFlakeSqlQueryPath: `${SnowFlakeSqlPath}vaccine_hpi`,
+    schemaPath: `../SQL/${SnowFlakeSqlPath}schema/`,
+    schemaInputPath: `${schemaPath}input/schema.json`,
+    schemaInputPath: `${schemaPath}input/schema.json`,
+    schemaOutputPath: `${schemaPath}output/schema.json`,
+    passingInputFile: `${schemaPath}input/pass/`,
+    passingOutputFile: `${schemaPath}output/pass/`,
+    metaDataObject: `${schemaPath}meta.json`, // Meta object describing the dataset
+  };
+};
 
+const nowPacTime = (options) =>
+  new Date().toLocaleString("en-CA", {
+    timeZone: "America/Los_Angeles",
+    ...options,
+  });
+
+const todayDateString = () =>
+  nowPacTime({ year: "numeric", month: "2-digit", day: "2-digit" });
+
+const todayTimeString = () =>
+  nowPacTime({
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).replace(/:/g, "-");
+
+/**
+ * Submit new data as pull request to GitHub.
+ * @returns Pull Request Q: what's the structure that's expected to be returned here?
+ */
 const doCovidVaccineHPI = async () => {
-    const gitModule = new GitHub({ token: process.env["GITHUB_TOKEN"] });
-    const gitRepo = await gitModule.getRepo(githubUser,githubRepo);
+  // Load local settings for this function.
+  const configGitHub = getConfigGitHub();
+  const configDataset = getConfigDataset();
 
-    const branchPrefix = 'data-vaccine-hpi-';
-    const commitMessage = 'Update Vaccine HPI Data';
-    const PrTitle = `${todayDateString()} Vaccine HPI`;
-    let branch = masterBranch;
+  const gitModule = new GitHub({ token: process.env["GITHUB_TOKEN"] });
+  const gitRepo = await gitModule.getRepo(
+    configGitHub.githubUser,
+    configGitHub.githubRepo
+  );
 
-    const prs = await gitRepo.listPullRequests({
-        base:masterBranch
-    });
-    let Pr = prs.data.filter(x=>x.title===PrTitle)[0];
+  const pullRequestTitle = `${todayDateString()} ${configDataset.datasetName}`;
 
-    if(Pr) { //reuse the PR if it is still open
-        branch = Pr.head.ref;
+  let branch = configGitHub.masterBranch;
+
+  const prs = await gitRepo.listPullRequests({
+    base: configGitHub.masterBranch,
+  });
+
+  let pullRequest = prs.data.filter((x) => x.title === pullRequestTitle)[0];
+
+  if (pullRequest) {
+    // Re-use the pull request if it is still open.
+    branch = pullRequest.head.ref;
+  }
+
+  // Current processed data object to write to GitHub if valid and different from previous files.
+  const dataOutput = await getData();
+
+  // Read content of file from GitHub branch.
+  const targetcontent = (
+    await gitRepo.getContents(
+      branch,
+      `${configGitHub.targetPath}${configGitHub.targetFileName}`,
+      true
+    )
+  ).data;
+
+  // Check if file has changed.
+  if (JSON.stringify(dataOutput.data) === JSON.stringify(targetcontent.data)) {
+    console.log("Data matched: no need to update");
+  } else {
+    console.log("Data changed: updating");
+    if (!pullRequest) {
+      // Create new branch
+      branch = `${
+        configGitHub.branchPrefix
+      }-${todayDateString()}-${todayTimeString()}`;
+      await gitRepo.createBranch(configGitHub.masterBranch, branch);
     }
 
-    const dataOutput = await getData();
-    const targetcontent = (await gitRepo.getContents(branch,`${targetPath}${targetFileName}`,true)).data;
-    if(JSON.stringify(dataOutput.data)===JSON.stringify(targetcontent.data)) {
-        console.log('data matched - no need to update');
-    } else {
-        console.log('data changed - updating');
-
-        if(!Pr) {
-            //new branch
-            branch = `${branchPrefix}-${todayDateString()}-${todayTimeString()}`;
-            await gitRepo.createBranch(masterBranch,branch);
-        }
-
-        await gitRepo.writeFile(branch, `${targetPath}${targetFileName}`, JSON.stringify(dataOutput,null,2), commitMessage, {committer,encode:true});
-
-        if(!Pr) {
-            //new Pr
-            Pr = (await gitRepo.createPullRequest({
-                title: PrTitle,
-                head: branch,
-                base: masterBranch
-            }))
-            .data;
-        }
-    }
-
-    //Approve the PR
-    if(Pr) {
-        await gitRepo.mergePullRequest(Pr.number,{
-            merge_method: 'squash'
-        });
-
-        await gitRepo.deleteRef(`heads/${Pr.head.ref}`);
-    }
-    return Pr;
-};
-
-
-const getData = async () => {
-    const sqlResults = await queryDataset(
-        {
-            data: getSQL(`${SnowFlakeSqlPath}vaccine_hpi`)
-        }
-        ,process.env["SNOWFLAKE_CDTCDPH_VACCINE"]
+    // Write file to GitHub branch.
+    await gitRepo.writeFile(
+      branch,
+      `${configGitHub.targetPath}${configGitHub.targetFileName}`,
+      JSON.stringify(dataOutput, null, 2),
+      configGitHub.commitMessage,
+      { committer: configGitHub.commitMessage, encode: true }
     );
 
-    validateJSON('vaccine-hpi.json failed validation', sqlResults.data,`${schemaPath}input/schema.json`,`${schemaPath}input/pass/`);
+    if (!pullRequest) {
+      // Create new pull request.
+      pullRequest = (
+        await gitRepo.createPullRequest({
+          title: pullRequestTitle,
+          head: branch,
+          base: configGitHub.masterBranch,
+        })
+      ).data;
+    }
+  }
 
-    let LATEST_ADMINISTERED_DATE = new Date("1900-01-01");
-    sqlResults.data.forEach(r=>{
-        if(LATEST_ADMINISTERED_DATE<r.LATEST_ADMIN_DATE) {
-            LATEST_ADMINISTERED_DATE = r.LATEST_ADMIN_DATE;
-        }
-
-        delete r.LATEST_ADMIN_DATE;
+  // Approve the pull request.
+  if (pullRequest) {
+    await gitRepo.mergePullRequest(pullRequest.number, {
+      merge_method: "squash",
     });
 
-    const mappedResults = { 
-        meta : {
-            LATEST_ADMINISTERED_DATE,
-            PUBLISHED_DATE : todayDateString()
-        },
-        data: sqlResults.data
-    };
-
-    validateJSON('vaccine-hpi.json failed validation', mappedResults,`${schemaPath}output/schema.json`,`${schemaPath}output/pass/`);
-
-    return mappedResults;
+    await gitRepo.deleteRef(`heads/${pullRequest.head.ref}`);
+  }
+  return pullRequest;
 };
 
+/**
+ *
+ * @param {*} param0
+ * @returns
+ */
+const getData = async () => {
+  
+  // Load local settings for this function.
+  const configGitHub = getConfigGitHub();
+  const configDataset = getConfigDataset();
+
+  // Load data.
+  const sqlResults = await queryDataset(
+    {
+      data: getSQL(`${configDataset.SnowFlakeSqlQueryPath}`),
+    },
+    configDataset.SNOWFLAKE_TOKEN
+  );
+
+  // Validate data.
+  validateJSON(
+    `${configGitHub.outputFileName} failed validation`,
+    sqlResults.data,
+    `${configDataset.schemaInputPath}`,
+    `${configDataset.passingInputFile}`
+  );
+
+  // Process special attributes.
+  let LATEST_ADMINISTERED_DATE = new Date("1900-01-01"); // Q: could this be null instead?
+
+  sqlResults.data.forEach((r) => {
+    if (LATEST_ADMINISTERED_DATE < r.LATEST_ADMIN_DATE) {
+      LATEST_ADMINISTERED_DATE = r.LATEST_ADMIN_DATE;
+    }
+
+    delete r.LATEST_ADMIN_DATE;
+  });
+
+  // Generate result date object.
+  const mappedResults = {
+    meta: getMeta({
+      {
+        LATEST_ADMINISTERED_DATE, // @TODO sync on converting this to lowercase.
+        PUBLISHED_DATE: todayDateString(), // @TODO sync on converting this to lowercase.
+        // latest_administered_date: LATEST_ADMINISTERED_DATE, // @TODO sync on converting this to lowercase.
+        // published_date: todayDateString(), // @TODO sync on converting this to lowercase.
+      })
+    data: sqlResults.data,
+  };
+
+  validateJSON(
+    `${configGitHub.outputFileName} failed validation`,
+    mappedResults,
+    `${configDataset.schemaOutputPath}`,
+    `${configDataset.passingOutputFile}`
+  );
+
+  return mappedResults;
+};
+
+const getMeta = (props) => {
+    // Load local settings for this function.
+    const configDataset = getConfigDataset();
+
+    let metaFile = configDataset.metaDataObject;
+}
+
 module.exports = {
-    doCovidVaccineHPI
+  doCovidVaccineHPI,
 };
