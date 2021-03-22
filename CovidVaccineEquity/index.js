befcb0c2e3ed5ef8632e3a49c7706ae96c2a462d
carterm <carter.medlin@gmail.com>
2021-03-22 10:24:24 -0700

    Readability

--- CovidVaccineEquity/index.js
+++ CovidVaccineEquity/index.js
@@ -5,26 +5,26 @@ const debugChannel = 'C01DBP67MSQ'; // testingbot
 
 module.exports = async function (context, myTimer) {
   const appName = context.executionContext.functionName;
-  let ts = null;
+  let slackPostTS = null;
   try {
-    ts = (await slackBotChatPost(debugChannel,`${appName} (Every 30 mins from 9:15am to 1:45pm)`)).json().ts;
+    slackPostTS = (await slackBotChatPost(debugChannel,`${appName} (Every 30 mins from 9:15am to 1:45pm)`)).json().ts;
 
     const PrResult = await doCovidVaccineEquity();
 
     if(PrResult) {
       const prMessage = `Vaccine equity data deployed\n${PrResult.html_url}`;
-      await slackBotReplyPost(debugChannel,ts,prMessage);
-      await slackBotChatPost(notifyChannel,prMessage);
+      await slackBotReplyPost(debugChannel, slackPostTS, prMessage);
+      await slackBotChatPost(notifyChannel, prMessage);
     }
 
-    await slackBotReplyPost(debugChannel,ts,`${appName} finished`);
-    await slackBotReactionAdd(debugChannel, ts, 'white_check_mark');
+    await slackBotReplyPost(debugChannel, slackPostTS,`${appName} finished`);
+    await slackBotReactionAdd(debugChannel, slackPostTS, 'white_check_mark');
   } catch (e) {
     await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
 
-    if(ts) {
-      await slackBotReplyPost(debugChannel,ts,`${appName} ERROR!`);
-      await slackBotReactionAdd(debugChannel, ts, 'x');
+    if(slackPostTS) {
+      await slackBotReplyPost(debugChannel, slackPostTS, `${appName} ERROR!`);
+      await slackBotReactionAdd(debugChannel, slackPostTS, 'x');
     }
   }
 };
