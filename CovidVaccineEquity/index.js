e3d0d8a79333613d8d6b6fd5af03c88c39a66e57
carterm <carter.medlin@gmail.com>
2021-03-22 10:14:18 -0700

    Bundling testingbot messages into single post

--- CovidVaccineEquity/index.js
+++ CovidVaccineEquity/index.js
@@ -1,22 +1,23 @@
 const { doCovidVaccineEquity } = require('./worker');
-const { slackBotChatPost, slackBotReportError } = require('../common/slackBot');
+const { slackBotChatPost, slackBotReportError, slackBotReplyPost, slackBotReactionAdd } = require('../common/slackBot');
 const notifyChannel = 'C01HTTNKHBM'; //covid19-vaccines
 const debugChannel = 'C01DBP67MSQ'; // testingbot
 
 module.exports = async function (context, myTimer) {
   const appName = context.executionContext.functionName;
   try {
-    await slackBotChatPost(debugChannel,`${appName} (Every 30 mins from 10:15am to 1:45pm)`);
+    const ts = (await slackBotChatPost(debugChannel,`${appName} (Every 30 mins from 9:15am to 1:45pm)`)).json().ts;
 
     const PrResult = await doCovidVaccineEquity();
 
     if(PrResult) {
       const prMessage = `Vaccine equity data deployed\n${PrResult.html_url}`;
-      await slackBotChatPost(debugChannel,prMessage);
+      await slackBotReplyPost(debugChannel,ts,prMessage);
       await slackBotChatPost(notifyChannel,prMessage);
     }
 
-    await slackBotChatPost(debugChannel,`${appName} finished`);
+    await slackBotReplyPost(debugChannel,ts,`${appName} finished`);
+    await slackBotReactionAdd(debugChannel, ts, 'white_check_mark');
   } catch (e) {
     await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
   }
