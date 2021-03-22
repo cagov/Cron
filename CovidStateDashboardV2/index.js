1fcffb21fc19e111bdb76e8a1e2f0ab294a4b40a
carterm <carter.medlin@gmail.com>
2021-03-22 12:24:32 -0700

    CovidStateDashboard V2

--- CovidStateDashboardV2/index.js
+++ CovidStateDashboardV2/index.js
@@ -1,24 +1,31 @@
 const { doCovidStateDashboarV2 } = require('./worker');
-const { slackBotChatPost, slackBotReportError } = require('../common/slackBot');
-const notifyChannel = 'C01AA1ZB05B';
-const debugChannel = 'C01DBP67MSQ'; // 'C01AA1ZB05B';
-
+const { slackBotChatPost, slackBotReportError, slackBotReplyPost, slackBotReactionAdd } = require('../common/slackBot');
+const notifyChannel = 'C01AA1ZB05B'; // #covid19-state-dash
+const debugChannel = 'C01DBP67MSQ'; // #testingbot
 
 module.exports = async function (context, myTimer) {
   const appName = context.executionContext.functionName;
+  let slackPostTS = null;
   try {
-    await slackBotChatPost(debugChannel,`${appName} (Every 30 mins from 10am to 1:30pm)`);
+    slackPostTS = (await (await slackBotChatPost(debugChannel,`${appName} (Every 30 mins from 9:21am to 11:51am)`)).json()).ts;
 
     const PrResult = await doCovidStateDashboarV2();
 
     if(PrResult) {
-      const prMessage = `Daily stats deployed\n${PrResult.html_url}`;
-      await slackBotChatPost(debugChannel,prMessage);
-      await slackBotChatPost(notifyChannel,prMessage);
+      const prMessage = `Daily stats V2 deployed\n${PrResult.html_url}`;
+      await slackBotReplyPost(debugChannel, slackPostTS, prMessage);
+      await slackBotReactionAdd(debugChannel, slackPostTS, 'package');
+      await slackBotChatPost(notifyChannel, prMessage);
     }
 
-    await slackBotChatPost(debugChannel,`${appName} finished`);
+    await slackBotReplyPost(debugChannel, slackPostTS,`${appName} finished`);
+    await slackBotReactionAdd(debugChannel, slackPostTS, 'white_check_mark');
   } catch (e) {
     await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
+
+    if(slackPostTS) {
+      await slackBotReplyPost(debugChannel, slackPostTS, `${appName} ERROR!`);
+      await slackBotReactionAdd(debugChannel, slackPostTS, 'x');
+    }
   }
 };
