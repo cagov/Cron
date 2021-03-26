ee417fe648f45a68d7132b52efca4b0a2d925b67
carterm <carter.medlin@gmail.com>
2021-03-26 09:56:23 -0700

    cleanup

--- Crondo/modules.js
+++ Crondo/modules.js
@@ -8,6 +8,28 @@ const notifyChannel_covid19_state_dash = 'C01AA1ZB05B'; // #covid19-state-dash
 const notifyChannel_covid19_blueprint = 'C019DS5S6Q2'; // #covid19-blueprint
 const notifyChannel_covid19_vaccines = 'C01HTTNKHBM'; //covid19-vaccines
 
+
+/**
+ * 
+ * @param {{html_url:string}} [prResult] PR if created
+ * @param {string} prMessageHeader 
+ * @param {string} [notifyChannel]
+ * @param {string} feedChannel 
+ * @param {string} slackPostTS 
+ */
+const slackPrResult = async (prResult, prMessageHeader,notifyChannel, feedChannel, slackPostTS)  => {
+  if(prResult) {
+    const prMessage = `${prMessageHeader}\n${prResult.html_url}`;
+    await slackBotReplyPost(feedChannel, slackPostTS, prMessage);
+    await slackBotReactionAdd(feedChannel, slackPostTS, 'package');
+    if(notifyChannel) {
+      await slackBotChatPost(notifyChannel, prMessage);
+    }
+  }
+};
+
+
+
 /**
  * Runs a CRON module by name
  * @param {string} moduleName The module to run
@@ -15,40 +37,26 @@ const notifyChannel_covid19_vaccines = 'C01HTTNKHBM'; //covid19-vaccines
  * @param {string} slackPostTS The Slack TimeStamp to post replies to
  */
 const runModule = async (moduleName, feedChannel, slackPostTS) => {
-  let prResult = null;
-  let prMessage = null;
-
   switch (moduleName) {
     case 'CovidStateDashboardV2':
-      prResult = await doCovidStateDashboarV2();
-
-      if(prResult) {
-        prMessage = `Daily stats V2 deployed\n${prResult.html_url}`;
-        await slackBotReplyPost(feedChannel, slackPostTS, prMessage);
-        await slackBotReactionAdd(feedChannel, slackPostTS, 'package');
-        await slackBotChatPost(notifyChannel_covid19_state_dash, prMessage);
-      }
+      await slackPrResult(await doCovidStateDashboarV2(),
+        'Daily stats V2 deployed',
+        notifyChannel_covid19_state_dash,
+        feedChannel,slackPostTS);
       return;
     case 'doCovidVaccineEquity':
-      prResult = await doCovidVaccineEquity();
-
-      if(prResult) {
-        prMessage = `Vaccine equity data deployed\n${prResult.html_url}`;
-        await slackBotReplyPost(feedChannel, slackPostTS, prMessage);
-        await slackBotReactionAdd(feedChannel, slackPostTS, 'package');
-        await slackBotChatPost(notifyChannel_covid19_vaccines, prMessage);
-      }
+      await slackPrResult(await doCovidVaccineEquity(),
+        'Vaccine equity data deployed',
+        notifyChannel_covid19_vaccines,
+        feedChannel,slackPostTS);
       return;
     case 'CovidVaccineHPIV2':
-      prResult = await doCovidVaccineHPIV2();
-
-      if(prResult) {
-        prMessage = `Vaccine HPI data deployed\n${prResult.html_url}`;
-        await slackBotReplyPost(feedChannel, slackPostTS, prMessage);
-        await slackBotReactionAdd(feedChannel, slackPostTS, 'package');
-        await slackBotChatPost(notifyChannel_covid19_vaccines, prMessage);
-      }
+      await slackPrResult(await doCovidVaccineHPIV2(),
+        'Vaccine HPI data deployed',
+        notifyChannel_covid19_vaccines,
+        feedChannel,slackPostTS);
       return;
+      /*
     case 'CovidWeeklyTierUpdate':
       (await doWeeklyUpdatePrs()).forEach(async p=>{
         prMessage = `Tier Update Deployed\n${p.html_url}`;
@@ -56,8 +64,8 @@ const runModule = async (moduleName, feedChannel, slackPostTS) => {
         await slackBotReactionAdd(feedChannel, slackPostTS, 'package');
         await slackBotChatPost(notifyChannel_covid19_blueprint, prMessage);
       });
-
-      return;
+        return;
+      */
     default:
       throw new Error(`no code match for ${moduleName}`);
   }
