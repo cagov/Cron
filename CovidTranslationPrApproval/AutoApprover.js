ba7e32efd0ec05f9a346a544c66651f148b60ee5
carterm <carter.medlin@gmail.com>
2021-04-26 12:00:15 -0700

    Initial start

---
+++ CovidTranslationPrApproval/AutoApprover.js
@@ -0,0 +1,48 @@
+const GitHub = require('github-api'); //https://github-tools.github.io/github/docs/3.2.3/Repository.html
+const githubUser = 'cagov';
+const githubRepo = 'covid19-static-data';
+
+//Check to see if we need stats update PRs, make them if we do.
+const doAutoApprover = async masterbranch => {
+    //https://github-tools.github.io/github/docs/3.2.3/Repository.html#listPullRequests
+    //https://developer.github.com/v3/pulls/#list-pull-requests
+
+    //https://api.github.com/repos/cagov/covid19/pulls?base=master
+
+    const gitModule = new GitHub({ token: process.env["GITHUB_TOKEN"] });
+    const gitRepo = await gitModule.getRepo(githubUser,githubRepo);
+    
+    const Prs = (await gitRepo.listPullRequests(
+        {
+            base : masterbranch,
+            direction : 'desc' //Newest ones first
+        }
+        ))
+        .data
+        .filter(p=>
+            !p.draft //ignore drafts
+            //&&p.labels.some(s=>s.name===labelFilter) //require the 'Translated Content' label
+        );
+    for (const prlist of Prs) {
+        //get the full pr detail
+        const pr = (await gitRepo.getPullRequest(prlist.number)).data;
+        if (pr.mergeable) {
+            //Grab all the checks running on this PR
+            const checks = (await gitRepo._request('GET',`/repos/${gitRepo.__fullname}/commits/${pr.head.sha}/check-runs`)).data;
+            const pass = checks.check_runs.every(x=>x && x.status==='completed' && x.conclusion==='success');
+
+            if (pass) {
+                //Approve the PR
+                await gitRepo.mergePullRequest(pr.number,{
+                    merge_method: 'squash'
+                });
+
+                await gitRepo.deleteRef(`heads/${pr.head.ref}`);
+            }
+        }
+    }
+};
+
+module.exports = {
+  doAutoApprover
+};
