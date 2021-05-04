18a8a100f43e22572712aa7d4f177bb295e44144
carterm <carter.medlin@gmail.com>
2021-05-04 14:16:57 -0700

    validation back on

--- CovidStateDashboardTables/worker.js
+++ CovidStateDashboardTables/worker.js
@@ -11,8 +11,8 @@ const committer = {
 };
 const masterBranch = 'main';
 const stagingBranch = 'CovidStateDashboardTables_Staging';
-const doInputValidation = false;
-const doOutputValidation = false;
+const doInputValidation = true;
+const doOutputValidation = true;
 const sqlRootPath = '../SQL/CDT_COVID/CovidStateDashboardTables/';
 const outputPath = 'data/dashboard';
 const regionList = ["California","Alameda","Alpine","Amador","Butte","Calaveras","Colusa","Contra Costa","Del Norte","El Dorado","Fresno","Glenn","Humboldt","Imperial","Inyo","Kern","Kings","Lake","Lassen","Los Angeles","Madera","Marin","Mariposa","Mendocino","Merced","Modoc","Mono","Monterey","Napa","Nevada","Orange","Placer","Plumas","Riverside","Sacramento","San Benito","San Bernardino","San Diego","San Francisco","San Joaquin","San Luis Obispo","San Mateo","Santa Barbara","Santa Clara","Santa Cruz","Shasta","Sierra","Siskiyou","Solano","Sonoma","Stanislaus","Sutter","Tehama","Trinity","Tulare","Tuolumne","Ventura","Yolo","Yuba"];
