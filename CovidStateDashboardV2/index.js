d9158f33ce9cc93103260095319e8bbf054ae73a
carterm <carter.medlin@gmail.com>
2021-05-11 15:14:17 -0700

    Time string

--- CovidStateDashboardV2/index.js
+++ CovidStateDashboardV2/index.js
@@ -7,7 +7,7 @@ module.exports = async function (context, myTimer) {
   const appName = context.executionContext.functionName;
   let slackPostTS = null;
   try {
-    slackPostTS = (await (await slackBotChatPost(debugChannel,`${appName} (Every day @ 8:00am)`)).json()).ts;
+    slackPostTS = (await (await slackBotChatPost(debugChannel,`${appName} (Every day @ 7:45am)`)).json()).ts;
 
     const PrResult = await doCovidStateDashboarV2();
 
