5df84336024d79dfe82d0c2829b44bc647c137f5
carterm <carter.medlin@gmail.com>
2021-05-28 14:02:51 -0700

    using slack api

--- Crondo/index.js
+++ Crondo/index.js
@@ -1,14 +1,5 @@
-
-const { 
-  slackBotChatPost, 
-  slackBotReportError, 
-  slackBotReplyPost, 
-  slackBotReactionAdd,  
-  slackBotChannelHistory,
-  slackBotTimeStampFromDate
- } = require('../common/slackBot');
-
- const moment = require('moment'); // https://momentjs.com/docs/#/use-it/node-js/
+const { WebClient } = require('@slack/web-api'); //https://slack.dev/node-slack-sdk/web-api
+const moment = require('moment'); // https://momentjs.com/docs/#/use-it/node-js/
 
 //const { runModule } = require('./modules');
 
@@ -22,12 +13,14 @@ const dataTimeZone = 'America/Los_Angeles';
 //const dataTimeZone = 'America/New_York';
 
 module.exports = async function () {
+  const slackWeb = new WebClient(process.env["SLACKBOT_TOKEN"]);
   try {
-    const startOfDataTimeStamp = slackBotTimeStampFromDate(moment().startOf('day'));
+    const startOfDataTimeStamp = moment().startOf('day')/1000;
 
     const TodayDayOfWeekCode = weekdayCodes[moment().tz(dataTimeZone).day()];
     
-    const slackData = await (await slackBotChannelHistory(feedChannel,`&oldest=${startOfDataTimeStamp}`)).json();
+    const slackData = await slackWeb.conversations.history({channel:feedChannel,oldest:startOfDataTimeStamp});
+    //await (await slackBotChannelHistory(feedChannel,`&oldest=${startOfDataTimeStamp}`)).json();
     for (const myjob of jobs.filter(x=>x.enabled&x.days.includes(TodayDayOfWeekCode))) {
       
         const runToday = moment.tz({hour:myjob.runat.hour,minute:myjob.runat.minute},dataTimeZone);
@@ -42,27 +35,27 @@ module.exports = async function () {
 
           let runPlease = !RuntimeThread;
           if(!RuntimeThread) {
-            RuntimeThread = await (await slackBotChatPost(feedChannel,myjob.title)).json();
+            RuntimeThread = await slackWeb.chat.postMessage({channel: feedChannel,text: myjob.title});
           }
           const slackPostTS = RuntimeThread.ts;
           try {
             if(runPlease) {
               //await runModule(func.name,feedChannel,slackPostTS);
-
-              await slackBotReplyPost(feedChannel, slackPostTS,`${myjob.title} finished`);
-              await slackBotReactionAdd(feedChannel, slackPostTS, 'white_check_mark');
+              await slackWeb.chat.postMessage({channel: feedChannel, text:`${myjob.title} finished`,thread_ts:slackPostTS});
+              await slackWeb.reactions.add({channel:feedChannel,timestamp:slackPostTS,name:'white_check_mark'});
             } else {
-              await slackBotReplyPost(feedChannel, slackPostTS,`${myjob.title} scanned`);
+              await slackWeb.chat.postMessage({channel: feedChannel,text:`${myjob.title} scanned`,thread_ts:slackPostTS});
             }
           } catch (e) {
             //Report on this error and allow movement forward
-            await slackBotReactionAdd(feedChannel, slackPostTS, 'x');
-            await slackBotReplyPost(feedChannel, slackPostTS, `\`\`\`${e.stack}\`\`\``);
+            await slackWeb.reactions.add({channel:feedChannel,timestamp:slackPostTS,name:'x'});
+            await slackWeb.chat.postMessage({channel: feedChannel,text:`\`\`\`${e}\`\`\``,thread_ts:slackPostTS
+            });
           }
       }
     }
   } catch (e) {
     //Someething in the overall system failed
-    await slackBotReportError(feedChannel,`Error running Crondo`,e);
+    await slackWeb.chat.postMessage({channel: feedChannel,text:`*Error running Crondo*\`\`\`${e}\`\`\``});
   }
 };
