8e13dec474d8699feb8574ef1e1c3eab228de340
carterm <carter.medlin@gmail.com>
2021-06-25 14:22:36 -0700

    Reducing PR to just library changes

--- WordpressSync/debug.js
+++
@@ -1,9 +0,0 @@
-//Loading environment variables
-const { Values } = require('../local.settings.json');
-Object.keys(Values).forEach(x=>process.env[x]=Values[x]); //Load local settings file for testing
-
-//run the indexpage async
-const indexCode = require('./index');
-(async () => {
-   return await indexCode();
-})();
