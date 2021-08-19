724e8565ca8e2cc61891426dc4991c9093860586
Jim Bumgardner <jbum@jbum.com>
2021-08-18 17:38:13 -0700

    Modified vaccines data to more closely mimic formats used by other sparklines.

--- CovidStateDashboardVaccines/daily-vaccines-sparkline.js
+++ CovidStateDashboardVaccines/daily-vaccines-sparkline.js
@@ -19,11 +19,26 @@ const getData_daily_vaccines_sparkline = async () => {
   // FILTER JSON HERE - remove fields we don't want
 
   // pull a subset of fields  here...
+  let mapped_sparkline_data = statResults.sparkline_data.map(r => {
+    return {DATE:r.ADMIN_DATE, VALUE:r.COUNT};
+  });
+
   let json = {
       meta: {
-        PUBLISHED_DATE: "1900-01-01"
+        PUBLISHED_DATE: "1900-01-01",
+        coverage: "California"
       },
-      data: statResults.sparkline_data
+      data: { 
+        time_series: {
+          VACCINE_DOSES: {
+            DATE_RANGE: {
+              MINIMUM: mapped_sparkline_data[mapped_sparkline_data.length-1].DATE,
+              MAXIMUM: mapped_sparkline_data[0].DATE
+            },
+            VALUES: mapped_sparkline_data
+          }
+        }
+      }
   };
 
   // For now, don't bother validating until we get data into a form we really like...
