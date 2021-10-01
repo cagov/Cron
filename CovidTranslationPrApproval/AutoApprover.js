5e349ec5bf45275b2ab5f3c406eed86185c549bf
Carter Medlin <carterm@users.noreply.github.com>
2021-10-01 14:13:19 -0700

    Rollup support

--- CovidTranslationPrApproval/AutoApprover.js
+++ CovidTranslationPrApproval/AutoApprover.js
@@ -1,3 +1,4 @@
+//@ts-check
 const GitHub = require('github-api'); //https://github-tools.github.io/github/docs/3.2.3/Repository.html
 const githubUser = 'cagov';
 const githubRepo = 'covid-static-data';
@@ -11,6 +12,34 @@ const AutoApproverLabels = require('./AutoApproverLabels.json').data;
 const labelPublishASAP = AutoApproverLabels.specialLabels.PublishASAP;
 const labelDoNotPublish = AutoApproverLabels.specialLabels.DoNotPublish;
 const securityGroups = ['OWNER','CONTRIBUTOR','COLLABORATOR'];
+const addToRollUptag = 'Add to Rollup';
+const rollUptag = 'Rollup';
+
+/**
+ * @typedef {Object} PrRow
+ * @property {string} html_url
+ * @property {string} title
+ * @property {number} number usually '100644'
+ * @property {[{name:string}]} labels usually 'blob'
+ * @property {boolean} draft
+ * @property {string} author_association
+ * @property {{ref:string}} base
+ * @property {{ref:string}} head
+ * @property {string} body
+ * @returns 
+ */
+
+/**
+ * @param {*} gitRepo 
+ */
+const getPrList = async gitRepo => {
+    return /** @type PrRow[] */ ((await gitRepo.listPullRequests())
+    .data)
+    .filter(p=>
+        !p.draft //ignore drafts
+        &&securityGroups.includes(p.author_association) //Security
+    );
+}
 
 /**
  * Check to see if we need stats update PRs, make them if we do.
@@ -29,10 +58,61 @@ const doAutoApprover = async () => {
     //https://api.github.com/repos/cagov/covid19/pulls?base=master
 
     const gitModule = new GitHub({ token: process.env["GITHUB_TOKEN"] });
+    // @ts-ignore
     const gitRepo = await gitModule.getRepo(githubUser,githubRepo);
+    // @ts-ignore
     const gitIssues = await gitModule.getIssues(githubUser,githubRepo);
 
+    // @ts-ignore
     moment.tz.setDefault(dataTimeZone); //So important when using Moment.JS
+
+
+    let PrList = await getPrList(gitRepo);
+    //Look for rollups
+    const PrRollUpList = PrList
+    .filter(p=>
+        p.labels.some(s=>s.name===addToRollUptag) //Only incude marked for rollup
+    );
+    for (const Pr of PrRollUpList) {
+        //See if there is a target roll up already
+        /** @type {{number:number,body:string,html_url:string}} */
+        let existingRollupPr = PrList.find(x=>x.labels.some(s=>s.name===rollUptag));
+
+        if(!existingRollupPr) {
+            //create a branch for rollup
+            const newBranchName = `${Pr.head.ref}-rollup-${new Date().valueOf()}`;
+            const newHead = await gitRepo.createBranch(Pr.head.ref,newBranchName);
+
+            /** @type {{html_url:string;number:number,body:string,head:{ref:string}}} */
+            const newPr = (await gitRepo.createPullRequest({
+                title: Pr.title + ' Rollup',
+                head: newBranchName,
+                base: Pr.base.ref,
+                body: '*This PR is a Rollup of multiple PRs...*\n\n'
+            }))
+            .data;
+
+            existingRollupPr = newPr;
+        }
+
+        let labels = Pr.labels
+            .map(x=>x.name)//Preserve existing labels!!!
+            .filter(x=>x!==addToRollUptag && x!==rollUptag);
+        labels.push(rollUptag);
+        
+        await gitIssues.editIssue(existingRollupPr.number,{
+            labels,
+            body: existingRollupPr.body + '\n\n' + Pr.title + ' - ' + Pr.html_url
+        });
+
+        //Close the old issue
+        await gitIssues.editIssue(Pr.number,{
+            state: 'closed',
+            body: Pr.body + '\n\nRolled into ' + existingRollupPr.html_url
+        });
+
+    }
+
 
     const ActiveLabels = AutoApproverLabels.timeLabels
         .map(x=>({label:x.label,diff:moment()
@@ -41,14 +121,15 @@ const doAutoApprover = async () => {
             .add(x.minute, 'minutes'),'minutes')}))
             .filter(x=>x.diff>0 && x.diff<15);
 
+
+    //refresh for tag fixing
+    PrList = await getPrList(gitRepo);
+
     for (const ActiveLabel of ActiveLabels) {
         //Mark any Prs with the time publish label as publish asap
-        const PrsTimeReady = (await gitRepo.listPullRequests())
-            .data
+        const PrsTimeReady = PrList
             .filter(p=>
-                !p.draft //ignore drafts
-                &&securityGroups.includes(p.author_association) //Security
-                &&p.labels.some(s=>s.name===ActiveLabel.label)
+                p.labels.some(s=>s.name===ActiveLabel.label)
             );
 
         //Time is up.  Mark it for publishing (in case we miss it this pass)
@@ -67,19 +148,14 @@ const doAutoApprover = async () => {
         await sleep(5000); //let label application apply
     }
 
-    const Prs = (await gitRepo.listPullRequests(
-        {
-            direction : 'desc' //Newest ones first
-        }
-        ))
-        .data
+    //refresh for approval
+    PrList = (await getPrList(gitRepo))
         .filter(p=>
-            !p.draft //ignore drafts
-            &&securityGroups.includes(p.author_association)
-            &&p.labels.some(s=>s.name===labelPublishASAP) //Only incude marked for publishing
+            p.labels.some(s=>s.name===labelPublishASAP) //Only incude marked for publishing
             &&!p.labels.some(s=>s.name===labelDoNotPublish) //Hard exclude DoNots (Stop button)
         );
-    for (const prlist of Prs) {
+
+    for (const prlist of PrList) {
         //get the full pr detail
         const pr = (await gitRepo.getPullRequest(`${prlist.number}?cachebust=${new Date().valueOf()}`)).data;
         if (pr.mergeable) {
