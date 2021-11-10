31f7fff74c03db60dc67bd7c883704e29956801a
Jim Bumgardner <jbum@jbum.com>
2021-11-09 18:37:42 -0800

    Modified eligible query.

--- common/SQL/CDTCDPH_VACCINE/statedashboard-vaccines/eligiblepopulation.sql
+++ common/SQL/CDTCDPH_VACCINE/statedashboard-vaccines/eligiblepopulation.sql
@@ -1,4 +1,5 @@
 -- Eligble population in california
 select sum(est_population) as "ELIGIBLE_POPULATION"
 from CA_VACCINE.CA_VACCINE.VW_CA_DOF_EST_COUNTY_POP_BY_SEX_RACE_AGE
-where age > 5;
+where age >= 5;
+
