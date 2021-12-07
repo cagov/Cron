abbdc583c71456d4cd508eff536b6b44bd82d4b1
Jim Bumgardner <jbum@jbum.com>
2021-12-07 11:06:27 -0800

    Using explicit fields in postvax query.

--- CovidPostvaxData/daily-postvax-data.js
+++ CovidPostvaxData/daily-postvax-data.js
@@ -15,23 +15,12 @@ const getData_daily_postvax_data = async () => {
 
   // validateJSON('CDTCDPH_VACCINE/Vaccines.sql failed validation', resultsVaccines,'../SQL/CDTCDPH_VACCINE/Vaccines.sql.Schema.json','../SQL/CDTCDPH_VACCINE/Vaccines.sql.Sample.json');
 
-  // FILTER JSON HERE - remove fields we don't want
-  const fields_to_remove = ['AREA','AREA_TYPE','FILE_PATH','SF_LOAD_TIMESTAMP']
-  let filtered_recs = statResults.postvax_data.map(r => {
-      fields_to_remove.forEach(f => {
-        if (f in r) {
-            delete r[f];
-        }
-      });
-    return r;
-  });
-
   // pull a subset of fields  here...
   let json = {
       meta: {
         PUBLISHED_DATE: "1900-01-01"
       },
-      data: filtered_recs
+      data: statResults.postvax_data
   };
 
   // For now, don't bother validating until we get data into a form we really like...
