6639ab1a45d2e0279aea0494b086cfc86f7a5602
Carter Medlin <carterm@users.noreply.github.com>
2022-01-05 12:17:57 -0800

    ready to start reporting more slack info

---
+++ CovidStateDashboardTablesTests/debug.js
@@ -0,0 +1,18 @@
+//Loading environment variables
+const { Values } = require("../local.settings.json");
+Object.keys(Values).forEach(x => (process.env[x] = Values[x])); //Load local settings file for testing
+
+process.env.debug = true; //set to false or remove to run like the real instance
+const repeatCount = parseInt(process.argv.slice(2));
+
+const context = { executionContext: { functionName: "debug" } };
+
+//run the indexpage async
+const indexCode = require("./index");
+
+(async () => {
+    for (let step = 0; step < repeatCount; step++) {
+        console.log(`****** Iteration ${step + 1} ******`);
+        await indexCode(context);
+    }
+})();
