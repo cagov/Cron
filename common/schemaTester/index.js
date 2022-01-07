bd0868a90ab17addd24998a859ef81c1d0c59383
Carter Medlin <carterm@users.noreply.github.com>
2022-01-06 17:32:06 -0800

    delta reduce

--- common/schemaTester/index.js
+++ common/schemaTester/index.js
@@ -1,60 +1,59 @@
 const fs = require('fs');
 
 const async_validator = require('./async_validator');
-const { threadWork } = require('./async_validator');;
+const { threadWork } = require('./async_validator');
 
 //https://json-schema.org/understanding-json-schema/
 //https://www.jsonschemavalidator.net/
 
 const getSqlWorkAndSchemas_getFileNames = passTestPath =>
-  (passTestPath.endsWith('/')
-    ? fs.readdirSync(passTestPath).map(testFile => passTestPath + testFile)
-    : [passTestPath])
-    .filter(f => fs.existsSync(f))
-    .map(testPath => ({
-      name: testPath.split('/').pop(),
-      json: JSON.parse(fs.readFileSync(testPath))
-    }));
+      (passTestPath.endsWith('/') 
+      ? fs.readdirSync(passTestPath).map(testFile=>passTestPath+testFile)
+      : [passTestPath])
+    .filter(f=>fs.existsSync(f))
+    .map (testPath=>({
+        name:testPath.split('/').pop(),
+        json:JSON.parse(fs.readFileSync(testPath))}));
 
 const getSqlWorkAndSchemas = (sqlPath, schemaPathFormat, PassTestPathFormat, FailTestPathFormat, outputSchemaPath) => {
   const sqlFullPath = `${__dirname}/${sqlPath}`;
   const sqlFiles = fs.readdirSync(sqlFullPath)
-    .filter(f => f.endsWith('.sql'))
-    .map(filename => ({ name: filename.replace(/\.sql$/, ''), filename, fullfilename: `${sqlFullPath}/${filename}` }));
+      .filter(f=>f.endsWith('.sql'))
+      .map(filename=>({name: filename.replace(/\.sql$/,''), filename, fullfilename:`${sqlFullPath}/${filename}`}));
 
   const JsonOutput = {
-    DbSqlWork: {},
-    schema: {},
+    DbSqlWork:{},
+    schema:{},
     outputSchema: []
   };
 
-  if (outputSchemaPath) {
-    JsonOutput.outputSchema = fs.readdirSync(sqlFullPath + outputSchemaPath)
-      .filter(f => f.endsWith('.json'))
-      .map(filename => ({ name: filename.replace(/\.json$/, ''), json: JSON.parse(fs.readFileSync(`${sqlFullPath}${outputSchemaPath}${filename}`)) }));
+  if(outputSchemaPath) {
+    JsonOutput.outputSchema = fs.readdirSync(sqlFullPath+outputSchemaPath)
+    .filter(f=>f.endsWith('.json'))
+    .map(filename=>({name: filename.replace(/\.json$/,''), json: JSON.parse(fs.readFileSync(`${sqlFullPath}${outputSchemaPath}${filename}`))}));
   }
 
 
-  sqlFiles.forEach(sql => {
+  sqlFiles.forEach(sql=>{
     JsonOutput.DbSqlWork[sql.name] = fs.readFileSync(sql.fullfilename).toString();
 
-    if (schemaPathFormat) {
-      const schemaPath = sqlFullPath + schemaPathFormat.replace(/\[file\]/, sql.name);
-      if (fs.existsSync(schemaPath)) {
+    if(schemaPathFormat) {
+      const schemaPath =  sqlFullPath + schemaPathFormat.replace(/\[file\]/,sql.name);
+      if(fs.existsSync(schemaPath)) {
 
         const newSchema = {
-          schema: JSON.parse(fs.readFileSync(schemaPath))
+          schema : JSON.parse(fs.readFileSync(schemaPath))
         };
 
-        if (PassTestPathFormat) {
-          let PassTestPath = sqlFullPath + PassTestPathFormat.replace(/\[file\]/, sql.name);
+        if(PassTestPathFormat) {
+          let PassTestPath = sqlFullPath + PassTestPathFormat.replace(/\[file\]/,sql.name);
           if (fs.existsSync(PassTestPath)) {
             newSchema.passTests = getSqlWorkAndSchemas_getFileNames(PassTestPath);
           }
         }
 
-        if (FailTestPathFormat) {
-          let FailTestPath = sqlFullPath + FailTestPathFormat.replace(/\[file\]/, sql.name);
+        if(FailTestPathFormat) {
+          let FailTestPath = sqlFullPath + FailTestPathFormat.replace(/\[file\]/,sql.name);
           if (fs.existsSync(FailTestPath)) {
             newSchema.failTests = getSqlWorkAndSchemas_getFileNames(FailTestPath);
           }
@@ -72,31 +71,31 @@ const getSqlWorkAndSchemas = (sqlPath, schemaPathFormat, PassTestPathFormat, Fai
 const validateJSON_getMessage = err =>
   `${err.stack} (${err.name}).\nValue = ${JSON.stringify(err.instance).substring(0, 50)}`;
 
-const validateJSON_getJsonFiles = path =>
-  (
-    fullpath =>
-      (fs.lstatSync(fullpath).isDirectory()
-        ? fs.readdirSync(fullpath)
-          .map(name => ({ name, fullfilename: `${fullpath}/${name}` }))
-        : [{ name: path, fullfilename: fullpath }])
-        .map(f => ({ name: f.name, json: JSON.parse(fs.readFileSync(f.fullfilename)) }))
-  )(`${__dirname}/${path}`);
+const validateJSON_getJsonFiles = path => 
+(
+  fullpath => 
+    (fs.lstatSync(fullpath).isDirectory()
+    ? fs.readdirSync(fullpath)
+      .map(name=>({name, fullfilename:`${fullpath}/${name}`}))
+    : [{name:path, fullfilename:fullpath}])
+      .map(f=>({name:f.name, json:JSON.parse(fs.readFileSync(f.fullfilename))}))
+)(`${__dirname}/${path}`);
 
-const mergeJSON = (target, stub) => {
-  if (stub === null || stub === undefined || typeof stub !== 'object') {
+const mergeJSON = (target,stub) => {
+  if(stub === null || stub === undefined || typeof stub !== 'object') {
     return stub;
   }
 
-  if (Array.isArray(stub)) {
+  if(Array.isArray(stub)) {
     const targetCopy = target === undefined || target === null ? [] : [...target]; //deep copy
-    stub.forEach((a, i) => {
-      targetCopy[i] = mergeJSON(targetCopy[i], stub[i]);
+    stub.forEach((a,i)=>{
+      targetCopy[i] = mergeJSON(targetCopy[i],stub[i]);
     });
     return targetCopy;
   } else {
-    const targetCopy = { ...target }; //deep copy
-    Object.keys(stub).forEach(k => {
-      targetCopy[k] = mergeJSON(targetCopy[k], stub[k]);
+    const targetCopy = {...target}; //deep copy
+    Object.keys(stub).forEach(k=>{
+      targetCopy[k] = mergeJSON(targetCopy[k],stub[k]);
     });
     return targetCopy;
   }
@@ -112,12 +111,12 @@ const mergeJSON = (target, stub) => {
  */
 const validateJSON = (errorMessagePrefix, targetJSON, schemafilePath, testGoodFilePath, testBadFilePath) => {
   validateJSON2(
-    errorMessagePrefix,
-    targetJSON,
-    require(schemafilePath),
-    testGoodFilePath ? validateJSON_getJsonFiles(testGoodFilePath) : null,
-    testBadFilePath ? validateJSON_getJsonFiles(testBadFilePath) : null
-  );
+      errorMessagePrefix,
+      targetJSON,
+      require(schemafilePath),
+      testGoodFilePath ? validateJSON_getJsonFiles(testGoodFilePath) : null,
+      testBadFilePath ? validateJSON_getJsonFiles(testBadFilePath) : null
+      );
 };
 
 
@@ -129,44 +128,44 @@ const validateJSON = (errorMessagePrefix, targetJSON, schemafilePath, testGoodFi
  * @param {[]} [testGoodFiles] Optional test data file that should pass
  * @param {[]} [testBadFiles] Optional test data file that should fail 
  */
-const validateJSON2 = (errorMessagePrefix, targetJSON, schemaJSON, testGoodFiles, testBadFiles) => {
+ const validateJSON2 = (errorMessagePrefix, targetJSON, schemaJSON, testGoodFiles, testBadFiles) => {
   const Validator = require('jsonschema').Validator; //https://www.npmjs.com/package/jsonschema
   const v = new Validator();
 
   if (testGoodFiles) {
     let latestGoodData = {};
     testGoodFiles
-      .forEach(({ name, json }) => {
+      .forEach(({name,json})=> {
         //console.log({name,json});
-        const r = v.validate(json, schemaJSON);
-
+        const r = v.validate(json,schemaJSON);
+  
         if (!r.valid) {
           logAndError(`${errorMessagePrefix} - Good JSON test is not 'Good' - ${validateJSON_getMessage(r.errors[0])} -  ${name}`);
         }
-
+  
         latestGoodData = json;
       }
-      );
-
-    if (testBadFiles) {
+    );
+  
+    if(testBadFiles) {
       testBadFiles
-        .forEach(({ name, json }) => {
+        .forEach(({name,json})=> {
           //console.log({name,json});
-          const merged = mergeJSON(latestGoodData, json);
-          const r = v.validate(merged, schemaJSON);
+          const merged = mergeJSON(latestGoodData,json);
+          const r = v.validate(merged,schemaJSON);
 
           if (r.valid) {
             logAndError(`${errorMessagePrefix} - Bad JSON test is not 'Bad' - ${name}`);
           }
         }
-        );
+      );
     }
   }
 
 
-  if (targetJSON) {
+  if(targetJSON) {
     //Reparse to simplify any Javascript objects like dates
-    const primaryResult = v.validate(JSON.parse(JSON.stringify(targetJSON)), schemaJSON);
+    const primaryResult = v.validate(JSON.parse(JSON.stringify(targetJSON)),schemaJSON);
 
     if (!primaryResult.valid) {
       logAndError(`${errorMessagePrefix} - ${validateJSON_getMessage(primaryResult.errors[0])}`);
@@ -180,25 +179,25 @@ const validateJSON2 = (errorMessagePrefix, targetJSON, schemaJSON, testGoodFiles
  * @param {threadWork[]} work An array of work to process
  * @param {number} max_threads Number of threads to process work.  Too many threads creates overhead.
  */
-const validateJSON_Async = async (errorMessagePrefix, work, max_threads) => new Promise(async (resolve, reject) =>
-  async_validator(work, max_threads)
-    .then(results => {
-      results.forEach(result => {
-        if (result.result.errors.length) {
-          const message = `${errorMessagePrefix} - ${result.name} - ${validateJSON_getMessage(result.result.errors[0])}`;
-          reject(message);
-          return;
-        }
-      });
-      resolve();
-    })
+ const validateJSON_Async = async (errorMessagePrefix, work, max_threads) => new Promise(async (resolve, reject) =>
+ async_validator(work, max_threads)
+   .then(results => {
+     results.forEach(result => {
+       if (result.result.errors.length) {
+         const message = `${errorMessagePrefix} - ${result.name} - ${validateJSON_getMessage(result.result.errors[0])}`;
+         reject(message);
+         return;
+       }
+     });
+     resolve();
+   })
 );
 
 /**
  * Logs an error message before throwing the message as an Error
  * @param {string} message Error message to display
  */
-const logAndError = message => {
+const logAndError  = message => {
   console.error(message);
   throw new Error(message);
 };
