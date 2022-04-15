e8a45887089655305cd1b0cbb220bb30754a0b2f
Jim Bumgardner <jbum@jbum.com>
2022-04-15 14:16:29 -0700

    Corrected method in CovidTestTrigger/debug.js.  Added two more debug.js files.

--- CovidVariantsDataPreview/debug.js
+++ CovidVariantsDataPreview/debug.js
@@ -3,16 +3,12 @@ const { Values } = require("../local.settings.json");
 Object.keys(Values).forEach(x => (process.env[x] = Values[x])); //Load local settings file for testing
 
 process.env.debug = true; //set to false or remove to run like the real instance
-const repeatCount = parseInt(process.argv.slice(2));
 
-const context = { executionContext: { functionName: "debug" }, 
-                  done: function() { console.log("Done function called"); } };
+const context = { executionContext: { functionName: "debug" } };
 
 //run the indexpage async
 const indexCode = require("./index");
 
 (async () => {
-    console.log("Testing beginning");
-    await indexCode(context,{method:'POST'});
-    console.log("Testing ending");
+    await indexCode(context);
 })();
