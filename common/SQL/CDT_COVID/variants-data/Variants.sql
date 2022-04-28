0b513f9d054d25a02f06ae0d12e47e4582484011
Carter Medlin <carterm@users.noreply.github.com>
2022-04-28 15:12:28 -0700

    hack 5

--- common/SQL/CDT_COVID/variants-data/Variants.sql
+++ common/SQL/CDT_COVID/variants-data/Variants.sql
@@ -3,7 +3,7 @@
 with main_q as
 (select *
 from COVID.DEVELOPMENT.VW_CDPH_COVID_VARIANTS) 
-select  DATE,VARIANT_NAME,METRIC_NAME,VALUE,REPORT_DATE from main_q
+select  DATExxxxxxx,VARIANT_NAME,METRIC_NAME,VALUE,REPORT_DATE from main_q
 where AREA='California' and  REPORT_DATE = (select max(REPORT_DATE) from main_q)
 ORDER BY DATE,VARIANT_NAME,METRIC_NAME
 ;
