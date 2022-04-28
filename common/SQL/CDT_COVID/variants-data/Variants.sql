27b60de1be8658fa2614fa68144993320b32c3b2
Carter Medlin <carterm@users.noreply.github.com>
2022-04-28 12:10:39 -0700

    adding field names to SQL

--- common/SQL/CDT_COVID/variants-data/Variants.sql
+++ common/SQL/CDT_COVID/variants-data/Variants.sql
@@ -1,11 +1,14 @@
 -- Data for COVID-19 Variants Charts
 
 with main_q as
-(select *
+(select 
+    VARIANT_NAME,
+    METRIC_NAME,
+    REPORT_DATE,
+    DATE,
+    VALUE
 from COVID.PRODUCTION.VW_CDPH_COVID_VARIANTS)
 select  DATE,VARIANT_NAME,METRIC_NAME,VALUE,REPORT_DATE from main_q
 where AREA='California' and  REPORT_DATE = (select max(REPORT_DATE) from main_q)
 ORDER BY DATE,VARIANT_NAME,METRIC_NAME
 ;
-
-
