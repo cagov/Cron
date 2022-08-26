80e0099d3f37cc68bd8117003f44934ea7fea7b4
Jim Bumgardner <jbum@jbum.com>
2022-08-26 09:52:56 -0700

    Improved messaging.

--- CovidPostvaxDataNoboost/index.js
+++ CovidPostvaxDataNoboost/index.js
@@ -9,7 +9,7 @@ module.exports = async function (context, myTimer) {
   const appName = context.executionContext.functionName;
   let slackPostTS = null;
   try {
-    slackPostTS = (await (await slackBotChatPost(debugChannel,`${appName} (Every Friday @ 7:35am (should snooze if not week-1))`)).json()).ts;
+    slackPostTS = (await (await slackBotChatPost(debugChannel,`${appName} (Every Friday @ 7:35am -- should snooze unless first week of month)`)).json()).ts;
     if (isIdleDay({weekends_off:true, holidays_off:true, first_week_only:true})) {
       await slackBotReplyPost(debugChannel, slackPostTS,`${appName} snoozed`);
       await slackBotReactionAdd(debugChannel, slackPostTS, 'zzz');
