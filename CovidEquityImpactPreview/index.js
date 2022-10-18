6a7e92ce7dd807c0a3529b891e6919bd7c54d831
James Logan <jamesstanleylogan@gmail.com>
2022-10-18 16:36:12 -0700

    move equity impact to monthly

--- CovidEquityImpactPreview/index.js
+++ CovidEquityImpactPreview/index.js
@@ -8,19 +8,23 @@ module.exports = async function (context, myTimer) {
 
   let slackPostTS = null;
   try {
-      slackPostTS = (await (await slackBotChatPost(debugChannel,`${appName} (Every Monday @ 8:10am)`)).json()).ts;
+    slackPostTS = (await (await slackBotChatPost(debugChannel,`${appName} (Every Monday @ 8:10am -- should snooze unless first Monday after a Friday)`)).json()).ts;
 
+    if (isIdleDay({weekends_off:true, holidays_off:true, check_first_mon_after_fri:true})) {
+      await slackBotReplyPost(debugChannel, slackPostTS,`${appName} snoozed`);
+      await slackBotReactionAdd(debugChannel, slackPostTS, 'zzz');
+    } else {
       const TreeRunResults = await doCovidEquityImpact(true); // preview run
 
       await slackBotReplyPost(debugChannel, slackPostTS,`${appName} finished`);
       await slackBotReactionAdd(debugChannel, slackPostTS, 'white_check_mark');
-
+    }
   } catch (e) {
-      await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
+    await slackBotReportError(debugChannel,`Error running ${appName}`,e,context,myTimer);
 
-      if(slackPostTS) {
-          await slackBotReplyPost(debugChannel, slackPostTS, `${appName} ERROR!`);
-          await slackBotReactionAdd(debugChannel, slackPostTS, 'x');
-      }
+    if(slackPostTS) {
+      await slackBotReplyPost(debugChannel, slackPostTS, `${appName} ERROR!`);
+      await slackBotReactionAdd(debugChannel, slackPostTS, 'x');
+    }
   }
 };
